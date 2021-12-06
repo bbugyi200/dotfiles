aeb179c0048346d7a83cfaae49b76a6e1dab7740
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-12-06 12:46:38 -0500

    Add macos pipx path to PATH

--- dot_config/macos.sh
+++ dot_config/macos.sh
@@ -5,3 +5,8 @@ alias del='brew remove'
 alias firefox='/Applications/Firefox.app/Contents/MacOS/firefox'
 alias get='brew install'
 alias qutebrowser='/Applications/qutebrowser.app/Contents/MacOS/qutebrowser'
+
+if cmd_exists python3; then
+    _pyver="$(python3 --version | perl -lanE 'print $F[1]' | perl -nE 'print s/([1-9]\.[1-9][0-9]*)\.[1-9][0-9]*/\1/gr')"
+    export PATH="$HOME/Library/Python/${_pyver}/bin/pipx:$PATH"
+fi
