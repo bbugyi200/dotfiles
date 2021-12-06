9a0f87806c17a0d3e1b330e9b90a00690b35c05f
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-12-06 12:09:15 -0500

    Add macos.sh file

---
+++ dot_config/macos.sh
@@ -0,0 +1,7 @@
+# ---------- MacOS Aliases / Functions ----------
+# def marker: MAC
+alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+alias del='brew remove'
+alias firefox='/Applications/Firefox.app/Contents/MacOS/firefox'
+alias get='brew install'
+alias qutebrowser='/Applications/qutebrowser.app/Contents/MacOS/qutebrowser'
