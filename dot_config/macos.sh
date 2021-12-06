8d82c38bb7e481e2bc74b380b5c60c64d2d55aea
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-12-06 12:47:35 -0500

    Use 'command -v' instead of 'cmd_exists'

--- dot_config/macos.sh
+++ dot_config/macos.sh
@@ -6,7 +6,7 @@ alias firefox='/Applications/Firefox.app/Contents/MacOS/firefox'
 alias get='brew install'
 alias qutebrowser='/Applications/qutebrowser.app/Contents/MacOS/qutebrowser'
 
-if cmd_exists python3; then
+if command -v python3 &>/dev/null; then
     _pyver="$(python3 --version | perl -lanE 'print $F[1]' | perl -nE 'print s/([1-9]\.[1-9][0-9]*)\.[1-9][0-9]*/\1/gr')"
     export PATH="$HOME/Library/Python/${_pyver}/bin/pipx:$PATH"
 fi
