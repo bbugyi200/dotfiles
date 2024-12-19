e2ef77fa75d5fbdb65d7f9942f94bffd1e6635f2
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-12-19 13:27:29 -0500

    Add .config/nvim/lua dir

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,1 +1,18 @@
 -- My (http://github.com/bbugyi200) Lua Configuration for NeoVim.
+
+---------- OPTIONS + VARIABLES
+-- Set Options
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Global Editor Variables
+vim.g.mapleader = ","
+
+---------- MAPPINGS
+-- Allow semilcolon (;) to be treated the same as colon (:).
+vim.keymap.set({'n', 'v'}, ';', ':')
+
+-- Maps to save / exit.
+vim.keymap.set({'n', 'i'}, '<leader>e', ':x!<cr>')
+vim.keymap.set({'n', 'i'}, '<leader>E', ':xa!<cr>')
+vim.keymap.set({'n', 'i'}, '<leader>s', ':update<cr>')
