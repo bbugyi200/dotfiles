b9ba9b826215a9d42e2e5d4d1e986df1ff5d1878
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-12-19 15:36:42 -0500

    Update neovim config - #8

--- dot_config/nvim/lua/plugins/nerdtree.lua
+++ dot_config/nvim/lua/plugins/nerdtree.lua
@@ -4,5 +4,8 @@ return {
     vim.g.NERDTreeWinSize = 60
     vim.keymap.set('n', '<LocalLeader>n', ":NERDTree <C-R>=escape(expand(\"%:p:h\"), '#')<CR><CR>")
     vim.keymap.set('n', '<LocalLeader>N', ':NERDTreeToggle<CR>')
+    vim.cmd([[
+      let g:NERDTreeCustomOpenArgs = {'file': {'reuse': 'all', 'where': 'p', 'keepopen': 0}, 'dir': {}}
+    ]])
   end,
 }
