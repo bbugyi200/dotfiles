1e398fce1a9bcdcd87a411ce1c54beaca028426e
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-12-21 09:07:16 -0500

    Clean up get_visual()

--- dot_config/nvim/lua/snip_utils.lua
+++ dot_config/nvim/lua/snip_utils.lua
@@ -6,15 +6,10 @@ local i = ls.insert_node
 
 -- Used to replicate UtilSnips ${VISUAL} variable.
 function M.get_visual(args, parent, _old_state, default)
-	if default == nil then
-		ret = ""
-	else
-		ret = default
-	end
 	if #parent.snippet.env.LS_SELECT_RAW > 0 then
 		return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
 	else
-		return sn(nil, i(1, ret))
+		return sn(nil, i(1, default or ""))
 	end
 end
 
