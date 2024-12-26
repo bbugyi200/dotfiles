613394d44983a4fd8072b4a30c75dcc9c7b1f192
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-12-26 15:05:45 -0500

    Remove nerdtree

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -4,9 +4,17 @@ return {
 	dependencies = "nvim-tree/nvim-web-devicons",
 	init = function()
 		vim.opt.termguicolors = true
-		require("bufferline").setup({})
+		require("bufferline").setup({
+			options = {
+				numbers = "buffer_id",
+				diagnostics = "nvim_lsp",
+				diagnostics_update_on_event = true,
+				diagnostics_indicator = function(count, _, _, _)
+					return "(" .. count .. ")"
+				end,
+				show_buffer_close_icons = false,
+				offsets = { filetype = "NvimTree", text = "File Explorer", text_align = "left", separator = true },
+			},
+		})
 	end,
-	opts = {
-		options = { numbers = "buffer_id" },
-	},
 }
