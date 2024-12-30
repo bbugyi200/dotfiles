2c36462733e3d48f3914a2cf5de1b711aef9e1ca
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-12-30 18:29:09 -0500

    Add maktaba

--- dot_config/nvim/lua/plugins/glugins.lua
+++ dot_config/nvim/lua/plugins/glugins.lua
@@ -3,6 +3,14 @@ local glug = require("glug")
 
 if funcs.on_google_machine() then
 	return {
+		-- maktaba is required by all google plugins
+		glug("maktaba", {
+			lazy = true,
+			dependencies = {},
+			config = function()
+				vim.cmd("source /usr/share/vim/google/glug/bootstrap.vim")
+			end,
+		}),
 		glug.glug("relatedfiles", {
 			cmd = "RelatedFilesWindow",
 			keys = {
