d66a19d542049372e67864be101b0fb0f9e49b4f
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-01-10 17:38:40 -0500

    Add cmp-nvim-lsp-signature-support plugin

--- dot_config/nvim/lua/plugins/nvim_cmp.lua
+++ dot_config/nvim/lua/plugins/nvim_cmp.lua
@@ -3,6 +3,7 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lsp-signature-help",
 		"hrsh7th/cmp-nvim-lua",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
