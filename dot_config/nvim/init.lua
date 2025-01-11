11f0cc3975797fb4f6b02c2f115f55d5d31b1572
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-01-11 13:53:02 -0500

    Add todo-comments plugin

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -16,48 +16,50 @@ require("config.lsp")
 require("config.postload")
 
 -- =============== TODO SOON ===============
--- TODO[ ]: Disable nvim notifications every time a config file changes!
--- TODO[ ]: Split config/lsp.lua into plugins/lspconfig.lua and plugins/nvim_cmp.lua!
--- TODO[ ]: Add `gV` map!
--- TODO[ ]: Install ALL plugins listed in go/neovim!
--- TODO[ ]: Flex critique plugins!
--- TODO[ ]: Use different keymap (NOT <cr>) for accepting completion!
--- TODO[ ]: Improve local vimrc
+-- TODO(bbugyi): Split config/lsp.lua into plugins/lspconfig.lua and plugins/nvim_cmp.lua!
+-- TODO(bbugyi): Add keymaps:
+--       * [ ] `gV`
+--       * [ ] `,<space>`
+--       * [ ] `,l` for `:Lazy`?
+-- TODO(bbugyi): Install ALL plugins listed in go/neovim!
+-- TODO(bbugyi): Flex critique plugins!
+-- TODO(bbugyi): Use different keymap (NOT <cr>) for accepting completion!
+-- TODO(bbugyi): Improve local vimrc
 --       * [ ] Add autocmd for $(pwd)/.vimrc.local
 --       * [ ] Move ~/.vimrc.local to ~/etc/vimrc?
--- TODO[ ]: Configure http://go/analysislsp-neovim !
--- TODO[ ]: Telescope extensions
+-- TODO(bbugyi): Telescope extensions
 --       * [ ] Install lots of Telescope extensions!
 --       * [ ] Use ,t<L> maps with Telescope builtins and extensions!
 --       * [ ] Find alternative to `:Telescope buffers` that favors most recent buffers.
--- TODO[ ]: Fix '-', '|', and '_' maps to default to lowest buffer num (NOT 1).
--- TODO[ ]: Install https://github.com/stevearc/conform.nvim for code formatting!
--- TODO[ ]: Create `autochez` script!
+-- TODO(bbugyi): Fix '-', '|', and '_' maps to default to lowest buffer num (NOT 1).
+-- TODO(bbugyi): Install https://github.com/stevearc/conform.nvim for code formatting!
+-- TODO(bbugyi): Create `autochez` script!
 
 -- =============== TODO LATER ===============
--- TODO[ ]: Add neocitc integrations described by https://team.git.corp.google.com/neovim-dev/neocitc/+/refs/heads/main
--- TODO[ ]: Write install/update script for building/installing NeoVim from source!
+-- TODO(bbugyi): Configure http://go/analysislsp-neovim !
+-- TODO(bbugyi): Add neocitc integrations described by https://team.git.corp.google.com/neovim-dev/neocitc/+/refs/heads/main
+-- TODO(bbugyi): Write install/update script for building/installing NeoVim from source!
 --          (CMD: make CMAKE_BUILD_TYPE=RelWithDebInfo -j && sudo make CMAKE=/opt/homebrew/bin/cmake install)
--- TODO[ ]: Move glug.lua to funcs/glug.lua?
--- TODO[ ]: Install https://github.com/sudormrfbin/cheatsheet.nvim ?
--- TODO[ ]: Install "vim-scripts/vcscommand.vim" with vcscommand-g4 as dep?
--- TODO[ ]: Add markdown LSP support!
--- TODO[ ]: Add fugitive keymaps!
--- TODO[ ]: Use tree splitter text objects (ex: cif keymap to clear and edit a function body)
--- TODO[ ]: Add on-the-fly luasnip snippet for TODOs in this file!
--- TODO[ ]: Make bufferline buffers use less space!
--- TODO[ ]: Install neovim only plugins you wanted to try.
+-- TODO(bbugyi): Move glug.lua to funcs/glug.lua?
+-- TODO(bbugyi): Install https://github.com/sudormrfbin/cheatsheet.nvim ?
+-- TODO(bbugyi): Install "vim-scripts/vcscommand.vim" with vcscommand-g4 as dep?
+-- TODO(bbugyi): Add markdown LSP support!
+-- TODO(bbugyi): Add fugitive keymaps!
+-- TODO(bbugyi): Use tree splitter text objects (ex: cif keymap to clear and edit a function body)
+-- TODO(bbugyi): Add on-the-fly luasnip snippet for TODOs in this file!
+-- TODO(bbugyi): Make bufferline buffers use less space!
+-- TODO(bbugyi): Install neovim only plugins you wanted to try.
 --       * [ ] https://github.com/mfussenegger/nvim-dap
--- TODO[ ]: Improve https://github.com/akinsho/bufferline.nvim!
+-- TODO(bbugyi): Improve https://github.com/akinsho/bufferline.nvim!
 --       * [ ] Group buffers by extension
 --       * [ ] Add :BufferLinePick map for splits and tabs!
 --       * [ ] Use :BufferLineCycleNext for ]b!
 --       * [ ] Add mappings to close all buffers, left buffers, and right buffers!
 --       * [ ] Use ordinal numbers instead of buffer numbers?
 --       * [ ] Figure out how to get diagnostics WITHOUT breaking highlighting!
--- TODO[ ]: Install https://github.com/zbirenbaum/copilot-cmp!
--- TODO[ ]: Test nvim built-in terminal support!
--- TODO[ ]: Install more completion sources
+-- TODO(bbugyi): Install https://github.com/zbirenbaum/copilot-cmp!
+-- TODO(bbugyi): Test nvim built-in terminal support!
+-- TODO(bbugyi): Install more completion sources
 --          (see https://github.com/hrsh7th/nvim-cmp/wiki/List-of-sources)!:
 --       * [ ] https://github.com/KadoBOT/cmp-plugins
 --       * [ ] https://github.com/zbirenbaum/copilot-cmp
@@ -65,7 +67,7 @@ require("config.postload")
 --       * [ ] https://github.com/andersevenrud/cmp-tmux
 --       * [ ] lspconfig.lua
 --       * [ ] cmp.lua
--- TODO[ ]: Configure Lua-Snips
+-- TODO(bbugyi): Configure Lua-Snips
 --       * [X] Migrate all useful 'all' snippets.
 --       * [ ] Add snippets for lua (ex: if, elif, ife, funcs, snippets, todo).
 --       * [ ] Migrate all useful Dart snippets.
@@ -74,9 +76,9 @@ require("config.postload")
 --       * [ ] Migrate all useful shell snippets.
 --       * [ ] Migrate all useful zorg snippets.
 --       * [ ] Create snippet that replaces `hc`!
--- TODO[ ]: Walk through vimrc line by line.
--- TODO[ ]: Walk through plugins.vim line by line.
--- TODO[ ]: Implement y* maps that copy parts of filename.
--- TODO[ ]: Merge config.luasnip into plugin.luasnip?
--- TODO[ ]: Configure clangd LSP server for work!
--- TODO[ ]: Get line/column number on bottom buffer tab back (with lualine?).
+-- TODO(bbugyi): Walk through vimrc line by line.
+-- TODO(bbugyi): Walk through plugins.vim line by line.
+-- TODO(bbugyi): Implement y* maps that copy parts of filename.
+-- TODO(bbugyi): Merge config.luasnip into plugin.luasnip?
+-- TODO(bbugyi): Configure clangd LSP server for work!
+-- TODO(bbugyi): Get line/column number on bottom buffer tab back (with lualine?).
---
+++ dot_config/nvim/lua/plugins/todo_comments.lua
@@ -0,0 +1,36 @@
+return {
+	"folke/todo-comments.nvim",
+	opts = {
+		highlight = {
+			multiline_pattern = [[^..  ]],
+			before = "", -- "fg" or "bg" or empty
+			keyword = "bg", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
+			after = "fg", -- "fg" or "bg" or empty
+			pattern = [[.*\s((KEYWORDS)%(\(bbugyi(200)?\))):]],
+			comments_only = true, -- uses treesitter to match keywords in comments only
+			max_line_len = 400, -- ignore lines longer than this
+			exclude = {}, -- list of file types to exclude highlighting
+		},
+		search = {
+			command = "rg",
+			args = {
+				"--glob=!*.snippets",
+				"--color=never",
+				"--no-heading",
+				"--with-filename",
+				"--line-number",
+				"--column",
+			},
+			pattern = [[\s(KEYWORDS)(\(bbugyi(200)?\)):]],
+		},
+	},
+	init = function()
+		vim.keymap.set("n", "]t", function()
+			require("todo-comments").jump_next()
+		end, { desc = "Next todo comment" })
+
+		vim.keymap.set("n", "[t", function()
+			require("todo-comments").jump_prev()
+		end, { desc = "Previous todo comment" })
+	end,
+}
