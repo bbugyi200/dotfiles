537bd2de37013ced2f2fccdfc43d1fec2f774a01
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-01-11 08:46:55 -0500

    Clean up comments

--- dot_config/nvim/lua/util/at_work.lua
+++ dot_config/nvim/lua/util/at_work.lua
@@ -1,6 +1,6 @@
 -- Check whether NeoVim is being run from a Google machine.
 --
----@return boolean # True iff I am on a work machine.
+---@return boolean # True if and only if I am on a work machine.
 local function at_work()
 	local handle = assert(io.popen("command -v /google/bin/releases/cider/ciderlsp/ciderlsp"))
 	local result = handle:read("*a")
