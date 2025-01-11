82d6c3d04c69f3ccd371f48705f1ae7f8afc76e0
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-01-11 09:57:52 -0500

    Improve doc comments again

--- dot_config/nvim/lua/config/postload.lua
+++ dot_config/nvim/lua/config/postload.lua
@@ -1,9 +1,8 @@
--- The Lua code in this file is loaded AFTER all other NeoVim configuration has been loaded.
+--- The Lua code in this file is loaded AFTER all other NeoVim configuration has been loaded.
 
--- Source {file}, if it exists.
---
+--- Source {file}, if it exists.
+---
 ---@param file string A *.vim or *.lua file to be sourced.
----@return nil
 local function source_if_exists(file)
 	local expanded = vim.fn.expand(file)
 	if vim.fn.filereadable(expanded) == 1 then
