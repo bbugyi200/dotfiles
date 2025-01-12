d02bb9c267bc91a9e8418f166c424abd45e981e7
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-01-12 11:42:28 -0500

    Fix is_goog_machine() import

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,4 +1,4 @@
-local is_goog_machine = require("is_goog_machine").is_goog_machine
+local is_goog_machine = require("util.is_goog_machine").is_goog_machine
 
 if is_goog_machine() then
 	-- When working from a Google machine, we use vvvv/ai.nvim instead of CoPilot.
