9aa7ca1019117e799a15afe7dde400e8bde27e1b
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-01-13 15:38:23 -0500

    Fix get_conform_opts()

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -26,21 +26,6 @@ end
 -- P4: Add doc comment with @param and @return annotations!
 local function get_conform_opts()
 	if is_goog_machine() then
-		return {
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				timeout_ms = 500,
-				lsp_format = "fallback",
-			},
-			formatters_by_ft = {
-				lua = { "stylua" },
-				-- Conform will run multiple formatters sequentially
-				python = { "isort", "black" },
-				-- You can customize some of the format options for the filetype (:help conform.format)
-				rust = { "rustfmt", lsp_format = "fallback" },
-			},
-		}
-	else
 		return function(_, opts)
 			local formatters_by_ft = {
 				borg = { "gclfmt" },
@@ -197,6 +182,21 @@ local function get_conform_opts()
 				},
 			})
 		end
+	else
+		return {
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				python = { "isort", "black" },
+				-- You can customize some of the format options for the filetype (:help conform.format)
+				rust = { "rustfmt", lsp_format = "fallback" },
+			},
+		}
 	end
 end
 
