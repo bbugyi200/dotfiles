93cee53a383de6546ea2b131b0909f6032704bd4
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-01-19 17:03:11 -0500

    Fix \n keymap so it can deactivate the tree again

--- dot_config/nvim/lua/plugins/nvim_tree.lua
+++ dot_config/nvim/lua/plugins/nvim_tree.lua
@@ -1,4 +1,3 @@
--- P0: Fix \n keymap so it can deactivate the tree again!
 -- P1: Remap 'g?' to '?' and sort by description by default!
 -- P3: Create zorg notes for best 'nvim-tree' keymaps!
 return {
@@ -6,7 +5,19 @@ return {
 	init = function()
 		require("nvim-tree").setup({})
 		vim.keymap.set("n", "<localleader>n", function()
-			vim.cmd(string.format("wincmd o | NvimTreeToggle %s", vim.fn.expand("%:h")))
+			local is_nvim_tree_open = false
+			for _, bufinfo in ipairs(vim.fn.getbufinfo()) do
+				---@type string
+				local buffer_name = bufinfo["name"]
+				if buffer_name:match("NvimTree") then
+					is_nvim_tree_open = true
+					break
+				end
+			end
+			if not is_nvim_tree_open then
+				vim.cmd("wincmd o")
+			end
+			vim.cmd("NvimTreeToggle " .. vim.fn.expand("%:h"))
 		end)
 	end,
 }
