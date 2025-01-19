b5bf4b14cfac5eda150ae8e2276476db75964e10
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-01-19 18:45:26 -0500

    Add vim-speeddating plugin

--- dot_config/nvim/lua/plugins/misc.lua
+++ dot_config/nvim/lua/plugins/misc.lua
@@ -1,4 +1,10 @@
+-- P1: Review https://nvimluau.dev/ for plugins to install!
+-- P1: Install https://github.com/stevearc/dressing.nvim ?
+-- P1: Install https://github.com/mhartington/oceanic-next colorscheme?
+-- P1: Install https://github.com/mrjones2014/legendary.nvim ?
 -- P1: Install https://github.com/xvzc/chezmoi.nvim !
+-- P1: Install https://github.com/tpope/vim-projectionist ?
+--   [ ] Use to switch between Dart alternate files!
 -- P2: Install https://github.com/otavioschwanck/arrow.nvim ?
 -- P2: Install https://github.com/stevearc/oil.nvim ?
 -- P3: Install https://github.com/sudormrfbin/cheatsheet.nvim ?
@@ -17,6 +23,7 @@ return {
 	"tpope/vim-fugitive",
 	"tpope/vim-repeat",
 	"tpope/vim-scriptease",
+	"tpope/vim-speeddating", -- For using <C-a> / <C-x> with dates and times.
 	"tpope/vim-surround",
 	"tpope/vim-unimpaired",
 	"tpope/vim-vinegar",
