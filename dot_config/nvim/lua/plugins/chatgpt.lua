72e492ac9c1296cb4acfbbddaa1cc6bc75d2651d
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-02-09 11:59:19 -0500

    Update plugins

--- dot_config/nvim/lua/plugins/chatgpt.lua
+++ dot_config/nvim/lua/plugins/chatgpt.lua
@@ -3,6 +3,7 @@
 local is_goog_machine = require("util.is_goog_machine").is_goog_machine
 
 if is_goog_machine() then
+	-- When working from a Google machine, I am not allowed to use ChatGPT.
 	return {}
 else
 	return {
