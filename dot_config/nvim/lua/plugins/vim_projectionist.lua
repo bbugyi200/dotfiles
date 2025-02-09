baae72913d497d382f3d2065e0dfe59cdf2ac790
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-02-09 13:52:49 -0500

    Sort projections

--- dot_config/nvim/lua/plugins/vim_projectionist.lua
+++ dot_config/nvim/lua/plugins/vim_projectionist.lua
@@ -12,33 +12,38 @@ return {
 		init = function()
 			vim.g.projectionist_heuristics = {
 				["*"] = {
-					["java/com/*.java"] = {
-						alternate = "javatests/com/{}Test.java",
-						type = "source",
-					},
-					["javatests/com/*Test.java"] = {
-						alternate = "java/com/{}.java",
-						type = "test",
-					},
+					-- css
 					["lib/*.scss"] = {
 						alternate = "lib/{}.dart",
 						type = "css",
 					},
+					-- html
 					["lib/*.acx.html"] = {
 						alternate = "lib/{}.dart",
 						type = "html",
+					},
+					-- po
+					["testing/lib/*_po.dart"] = {
+						alternate = "lib/{}.dart",
+						type = "po",
+					},
+					-- source
+					["java/com/*.java"] = {
+						alternate = "javatests/com/{}Test.java",
+						type = "source",
 					},
 					["lib/*.dart"] = {
 						alternate = { "lib/{}.acx.html", "lib/{}.scss", "test/{}_test.dart", "testing/lib/{}_po.dart" },
 						type = "source",
 					},
+					-- test
+					["javatests/com/*Test.java"] = {
+						alternate = "java/com/{}.java",
+						type = "test",
+					},
 					["test/*_test.dart"] = {
 						alternate = "lib/{}.dart",
 						type = "test",
-					},
-					["testing/lib/*_po.dart"] = {
-						alternate = "lib/{}.dart",
-						type = "po",
 					},
 				},
 			}
