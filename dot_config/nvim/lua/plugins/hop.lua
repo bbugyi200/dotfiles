8664dbc8d33eb080f80f6481686990089439265e
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-02-12 17:29:49 -0500

    Use leap instead of hop

--- dot_config/nvim/lua/plugins/hop.lua
+++
@@ -1,6 +0,0 @@
---- Neovim motions on speed!
-
-return {
-	-- PLUGIN: http://github.com/smoka7/hop.nvim
-	{ "smoka7/hop.nvim", opts = { yank_register = "+" } },
-}
---
+++ dot_config/nvim/lua/plugins/leap.lua
@@ -0,0 +1,6 @@
+--- Neovim motions on speed!
+
+return {
+	-- PLUGIN: http://github.com/ggandor/leap.nvim
+	{ "smoka7/hop.nvim", opts = {} },
+}
