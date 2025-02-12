e8ab049bba361eccf6664551644645eeac89c33e
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-02-12 16:12:05 -0500

    Make {} keymaps global

--- dot_config/nvim/lua/plugins/aerial.lua
+++ dot_config/nvim/lua/plugins/aerial.lua
@@ -10,23 +10,16 @@ return {
 			"nvim-tree/nvim-web-devicons",
 		},
 		init = function()
+			-- KEYMAP(N): {
+			vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { desc = "Jump to previous Aerial symbol." })
+			-- KEYMAP(N): }
+			vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { desc = "Jump to next Aerial symbol." })
 			-- KEYMAP(N): <leader>o
 			vim.keymap.set("n", "<leader>o", "<cmd>AerialToggle<cr>", {
 				desc = "Toggle aerial.nvim outline window.",
 			})
 			-- KEYMAP(N): <leader>to
 			vim.keymap.set("n", "<leader>to", "<cmd>Telescope aerial<cr>", { desc = "Telescope aerial" })
-		end,
-		on_attach = function(bufnr)
-			-- KEYMAP(N): {
-			vim.keymap.set(
-				"n",
-				"{",
-				"<cmd>AerialPrev<CR>",
-				{ buffer = bufnr, desc = "Jump to previous Aerial symbol." }
-			)
-			-- KEYMAP(N): }
-			vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr, desc = "Jump to next Aerial symbol." })
 		end,
 	},
 }
