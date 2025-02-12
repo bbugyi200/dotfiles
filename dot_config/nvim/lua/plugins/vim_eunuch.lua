e2c2ae57974039228eb258290e0bf41527537132
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-02-12 18:38:13 -0500

    Add support for renaming the current file using keymaps

--- dot_config/nvim/lua/plugins/vim_eunuch.lua
+++ dot_config/nvim/lua/plugins/vim_eunuch.lua
@@ -1,5 +1,12 @@
 --- Vim sugar for the UNIX shell commands that need it the most.
 
+--- Type ':Move <path>' to make it easier to rename files quickly.
+---
+---@param path string The ':Move' command's argument will be prepopulated with this path.
+local function preload_move_command(path)
+	vim.api.nvim_feedkeys(":Move " .. path, "n", true)
+end
+
 return {
 	-- PLUGIN: http://github.com/tpope/vim-eunuch
 	{
@@ -10,6 +17,19 @@ return {
 				-- '#!/bin/bash' and make the file executable!
 				["."] = "/bin/bash",
 			}
+
+			-- KEYMAP(N): <leader>md
+			vim.keymap.set("n", "<leader>md", function()
+				preload_move_command(vim.fn.expand("%:h") .. "/")
+			end, { desc = ":Move <path>/" })
+			-- KEYMAP(N): <leader>me
+			vim.keymap.set("n", "<leader>me", function()
+				preload_move_command(vim.fn.expand("%:r"))
+			end, { desc = ":Move <path>/<stem>" })
+			-- KEYMAP(N): <leader>mm
+			vim.keymap.set("n", "<leader>mm", function()
+				preload_move_command(vim.fn.expand("%"))
+			end, { desc = ":Move <path>/<stem>.<ext>" })
 		end,
 	},
 }
