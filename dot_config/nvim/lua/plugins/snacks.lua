ef6a3dd34f17f70bd46beea14024572d5e9a47b7
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-02-15 12:55:50 -0500

    Install snacks.nvim with 'input' snack enabled

---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,10 @@
+--- A collection of QoL plugins for Neovim
+--
+-- P2: Explore and install "snack" plugins using https://github.com/folke/snacks.nvim ?!
+
+return {
+	-- PLUGIN: http://github.com/folke/snacks.nvim
+	{ "folke/snacks.nvim", opts = {
+		input = { enabled = true },
+	} },
+}
