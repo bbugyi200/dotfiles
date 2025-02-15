13a5028e9c83a2adc5ee8d4639f68547041f9b5a
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-02-15 18:17:11 -0500

    Add wilder highlights

--- dot_config/nvim/lua/plugins/wilder.lua
+++ dot_config/nvim/lua/plugins/wilder.lua
@@ -4,6 +4,7 @@ return {
 	-- PLUGIN: http://github.com/gelguy/wilder.nvim
 	{
 		"gelguy/wilder.nvim",
+		dependencies = { "romgrk/fzy-lua-native" },
 		opts = {
 			modes = { ":", "/", "?" },
 		},
@@ -16,8 +17,16 @@ return {
 
 			local popupmenu_renderer = wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
 				border = "rounded",
+				pumblend = 20,
 				empty_message = wilder.popupmenu_empty_message_with_spinner(),
 				highlighter = highlighters,
+				highlights = {
+					accent = wilder.make_hl(
+						"WilderAccent",
+						"Pmenu",
+						{ { a = 1 }, { a = 1 }, { foreground = "#f4468f" } }
+					),
+				},
 				left = {
 					" ",
 					wilder.popupmenu_devicons(),
