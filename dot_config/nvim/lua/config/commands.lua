9552592d2a78b0bea987706f83b4940c5a99bf22
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-02-18 14:52:52 -0500

    Add :Help user command

---
+++ dot_config/nvim/lua/config/commands.lua
@@ -0,0 +1,20 @@
+--- Command-line abbrevations and custom user commands live here.
+
+-- KEYMAP(C): %% (Expand %% to <dir>/)
+vim.cmd("cnoremap <expr> %% getcmdtype() == ':' ? expand('%:h').'/' : '%%'")
+
+-- KEYMAP(ABBREVIATION): :// --> :e <dir>/
+vim.cmd("cnoreabbrev / <c-r>=getcmdpos() == 1 && getcmdtype() == ':' ? 'e ' . expand('%:h') : '/'<CR>")
+
+-- KEYMAP(C): :h --> :help
+vim.cmd("cnoreabbrev h <c-r>=getcmdpos() == 1 && getcmdtype() == ':' ? 'Help' : 'h'<CR>")
+vim.api.nvim_create_user_command("Help", "vsplit | help <args> | wincmd w | wincmd c", {
+	nargs = 1,
+	complete = "help",
+})
+
+-- KEYMAP(C): :o --> :Explore
+vim.cmd("cnoreabbrev o <c-r>=getcmdpos() == 1 && getcmdtype() == ':' ? 'Explore' : 'o'<CR>")
+
+-- KEYMAP(C): :v --> :verbose
+vim.cmd("cnoreabbrev v <c-r>=getcmdpos() == 1 && getcmdtype() == ':' ? 'verbose' : 'v'<CR>")
