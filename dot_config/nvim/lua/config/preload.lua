5a7ca39332cf1cfb2da37f5b5c1ae9ce741ebd88
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-02-18 14:54:32 -0500

    Set python3_host_prog to venv if it exists

--- dot_config/nvim/lua/config/preload.lua
+++ dot_config/nvim/lua/config/preload.lua
@@ -3,3 +3,10 @@
 -- Set <leader> and <localleader> keys.
 vim.g.mapleader = ","
 vim.g.maplocalleader = "\\"
+
+-- Set the Python 3 host program for Neovim. This is necessary in cases where I
+-- am unable to run `pip install pynvim` in the system Python 3 environment.
+local pyenv_python_bin = vim.env.HOME .. "/.pyenv/versions/neovim/bin/python"
+if vim.fn.filereadable(pyenv_python_bin) then
+	vim.g.python3_host_prog = pyenv_python_bin
+end
