8e0e079bb06d5a21353934b7a4649e969b1a1887
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-02-19 14:30:13 -0500

    Finish configuring Neogen

--- dot_config/nvim/lua/plugins/neogen.lua
+++ dot_config/nvim/lua/plugins/neogen.lua
@@ -2,5 +2,35 @@
 
 return {
 	-- PLUGIN: http://github.com/danymat/neogen
-	{ "danymat/neogen", opts = {} },
+	{
+		"danymat/neogen",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		opts = { snippet_engine = "luasnip" },
+		init = function()
+			-- KEYMAP(N): <leader>nn
+			vim.keymap.set(
+				"n",
+				"<leader>nn",
+				"<cmd>Neogen<cr>",
+				{ desc = "Generate doc comment based on context of current line." }
+			)
+
+			-- KEYMAP(N): <leader>nc
+			vim.keymap.set("n", "<leader>nc", "<cmd>Neogen class<cr>", { desc = "Generate doc comment for class." })
+
+			-- KEYMAP(N): <leader>nf
+			vim.keymap.set(
+				"n",
+				"<leader>nf",
+				"<cmd>Neogen function<cr>",
+				{ desc = "Generate doc comment for function." }
+			)
+
+			-- KEYMAP(N): <leader>nm
+			vim.keymap.set("n", "<leader>nm", "<cmd>Neogen file<cr>", { desc = "Generate doc comment for module." })
+
+			-- KEYMAP(N): <leader>nt
+			vim.keymap.set("n", "<leader>nt", "<cmd>Neogen type<cr>", { desc = "Generate doc comment for type." })
+		end,
+	},
 }
