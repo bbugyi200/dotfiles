5220fa497ad3053acdd8ca958b93405a764e60c3
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-02-20 16:32:14 -0500

    Install toggleterm plugin

---
+++ dot_config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,6 @@
+--- A neovim lua plugin to help easily manage multiple terminal windows.
+
+return {
+	-- PLUGIN: http://github.com/akinsho/toggleterm.nvim
+	{ "akinsho/toggleterm.nvim", opts = {} },
+}
