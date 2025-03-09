3ea65b81dfbe0172499a3b55c3f0083bd0c4a278
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-09 15:34:13 -0400

    Use 'config' instead of 'opts'

--- dot_config/nvim/lua/plugins/coverage.lua
+++ dot_config/nvim/lua/plugins/coverage.lua
@@ -4,7 +4,9 @@ return {
 	-- PLUGIN: http://github.com/andythigpen/nvim-coverage
 	{
 		"andythigpen/nvim-coverage",
-		opts = {},
+		config = function()
+			require("coverage").setup()
+		end,
 		dependencies = "nvim-lua/plenary.nvim",
 		cmd = {
 			"Coverage",
