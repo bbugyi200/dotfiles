ce2aa6008c285d2942a48b1d766d66b34c8c7436
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-08 21:34:09 -0500

    Gate gp.lua with is_goog_machine()

--- dot_config/nvim/lua/plugins/gp.lua
+++ dot_config/nvim/lua/plugins/gp.lua
@@ -3,7 +3,15 @@
 --- Gp.nvim (GPT prompt) Neovim AI plugin: ChatGPT sessions & Instructable
 --- text/code operations & Speech to text [OpenAI, Ollama, Anthropic, ..].
 
-return {
-	-- PLUGIN: http://github.com/Robitx/gp.nvim
-	{ "Robitx/gp.nvim", opts = { openai_api_key = "pass show chatgpt_nvim_api_key" } },
-}
+local is_goog_machine = require("util.is_goog_machine").is_goog_machine
+
+if is_goog_machine() then
+	-- When working from a Google machine, I am not allowed to use external LLM
+	-- tools like OpenAI or Anthropic.
+	return {}
+else
+	return {
+		-- PLUGIN: http://github.com/Robitx/gp.nvim
+		{ "Robitx/gp.nvim", opts = { openai_api_key = "pass show chatgpt_nvim_api_key" } },
+	}
+end
