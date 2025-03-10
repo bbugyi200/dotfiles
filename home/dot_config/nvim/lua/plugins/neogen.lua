f3495e63fc419e91100132b3fe91002a716f68a4
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-10 13:39:11 -0400

    Migrate ,ngn to ,ngg

--- home/dot_config/nvim/lua/plugins/neogen.lua
+++ home/dot_config/nvim/lua/plugins/neogen.lua
@@ -7,19 +7,19 @@ return {
 		dependencies = "nvim-treesitter/nvim-treesitter",
 		opts = { snippet_engine = "luasnip" },
 		init = function()
-			-- KEYMAP(N): <leader>ngn
-			vim.keymap.set(
-				"n",
-				"<leader>ngn",
-				"<cmd>Neogen<cr>",
-				{ desc = "Generate doc comment based on context of current line." }
-			)
-
 			-- KEYMAP(N): <leader>ngc
 			vim.keymap.set("n", "<leader>ngc", "<cmd>Neogen class<cr>", { desc = "Generate doc comment for class." })
 
 			-- KEYMAP(N): <leader>ngf
 			vim.keymap.set("n", "<leader>ngf", "<cmd>Neogen func<cr>", { desc = "Generate doc comment for function." })
+
+			-- KEYMAP(N): <leader>ngg
+			vim.keymap.set(
+				"n",
+				"<leader>ngg",
+				"<cmd>Neogen<cr>",
+				{ desc = "Generate doc comment based on context of current line." }
+			)
 
 			-- KEYMAP(N): <leader>ngm
 			vim.keymap.set("n", "<leader>ngm", "<cmd>Neogen file<cr>", { desc = "Generate doc comment for module." })
