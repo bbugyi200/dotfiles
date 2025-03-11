d46a0820fad5944bb6dd99fe945dc96ca1687108
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-11 13:25:04 -0400

    Expand := to :lua=

--- home/dot_config/nvim/lua/config/commands.lua
+++ home/dot_config/nvim/lua/config/commands.lua
@@ -2,13 +2,16 @@
 --
 -- P1: Write util/*.lua function for abbreviations!
 
--- KEYMAP(C): %% (Expand %% to <dir>/)
+-- ABBREVIATION: %% (Expand %% to <dir>/)
 vim.cmd("cnoremap <expr> %% getcmdtype() == ':' ? expand('%:h').'/' : '%%'")
 
--- KEYMAP(ABBREVIATION): :// --> :e <dir>/
+-- ABBREVIATION: = (Expand := to :lua=)
+vim.cmd("cnoreabbrev = <c-r>=getcmdpos() == 1 && getcmdtype() == ':' ? 'lua=' : '='<CR>")
+
+-- ABBREVIATION: :// --> :e <dir>/
 vim.cmd("cnoreabbrev / <c-r>=getcmdpos() == 1 && getcmdtype() == ':' ? 'e ' . expand('%:h') : '/'<CR>")
 
--- KEYMAP(C): :h --> :help
+-- ABBREVIATION: :h --> :help
 vim.cmd("cnoreabbrev h <c-r>=getcmdpos() == 1 && getcmdtype() == ':' ? 'vert help' : 'h'<CR>")
 vim.cmd("cnoreabbrev H <c-r>=getcmdpos() == 1 && getcmdtype() == ':' ? 'Help' : 'H'<CR>")
 vim.api.nvim_create_user_command("Help", "help <args> | wincmd w | wincmd c", {
@@ -17,8 +20,8 @@ vim.api.nvim_create_user_command("Help", "help <args> | wincmd w | wincmd c", {
 	complete = "help",
 })
 
--- KEYMAP(C): :o --> :Explore
+-- ABBREVIATION: :o --> :Explore
 vim.cmd("cnoreabbrev o <c-r>=getcmdpos() == 1 && getcmdtype() == ':' ? 'Explore' : 'o'<CR>")
 
--- KEYMAP(C): :v --> :verbose
+-- ABBREVIATION: :v --> :verbose
 vim.cmd("cnoreabbrev v <c-r>=getcmdpos() == 1 && getcmdtype() == ':' ? 'verbose' : 'v'<CR>")
