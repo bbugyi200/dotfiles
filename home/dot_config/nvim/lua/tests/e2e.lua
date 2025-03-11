33d95a57848e9629b846b6e81ed8ac446164a9df
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-11 17:26:17 -0400

    Add e2e test that sources config.* modules!

--- home/dot_config/nvim/lua/tests/e2e.lua
+++ home/dot_config/nvim/lua/tests/e2e.lua
@@ -1,6 +1,46 @@
+local after_each = require("busted").after_each
+local assert = require("busted").assert
+local before_each = require("busted").before_each
 local describe = require("busted").describe
+local it = require("busted").it
 local pending = require("busted").pending
 
 describe("End-to-end test that", function()
-	pending("sources nvim/init.lua, and verify that no errors are thrown")
+	local nvim -- Channel of the embedded Neovim process
+
+	before_each(function()
+		-- Start a new Neovim process
+		nvim = vim.fn.jobstart({ "nvim", "--embed", "--headless" }, { rpc = true, width = 80, height = 24 })
+		local nvim_lua_path = vim.fn.getcwd() .. "/home/dot_config/nvim"
+		vim.fn.rpcrequest(nvim, "nvim_command", "set runtimepath+=" .. nvim_lua_path)
+	end)
+
+	after_each(function()
+		-- Terminate the Neovim process
+		vim.fn.jobstop(nvim)
+	end)
+
+	it("sources each module in the config.* namespace", function()
+		local config_modules = {
+			"autocmds",
+			"commands",
+			"keymaps",
+			"load_local_configs",
+			"options",
+			"preload",
+		}
+
+		for _, module in ipairs(config_modules) do
+			local result = vim.fn.rpcrequest(nvim, "nvim_cmd", {
+				cmd = "lua",
+				args = { string.format("vim.print(pcall(require, 'config.%s'))", module) },
+				---@diagnostic disable-next-line: redundant-parameter
+			}, { output = true })
+			local ok, ret = string.match(result, "(%S+)%s*(.*)")
+			assert.is_equal(ok, "true", "ERROR IN 'config." .. module .. "' MODULE: " .. ret)
+			assert.is_equal(ret, "true")
+		end
+	end)
+
+	pending("sources each module in the plugins.* namespace")
 end)
--- home/dot_config/nvim/lua/tests/unit.lua
+++ home/dot_config/nvim/lua/tests/unit.lua
@@ -3,4 +3,5 @@ local pending = require("busted").pending
 
 describe("Unit test that", function()
 	pending("flexes util.delete_file()")
+	pending("flexes util.copy_to_clipboard()")
 end)
