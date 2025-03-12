4fdd42331a4a39e336844ef193f1f3edeeb44980
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-12 15:30:17 -0400

    Add luarocks install script

---
+++ home/.chezmoiscripts/run_script_utils.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BUILD_DIR=$HOME/tmp/chezmoi_build
+
+# Log message to stdout.
+#
+# Arguments:
+# ----------
+# msg: The message to print.
+# fmt_args: (optional) Arguments to format the message using printf.
+function util::log() {
+  local msg
+  if [[ "$#" -eq 1 ]]; then
+    msg="$1"
+    shift
+  else
+    msg="$(printf "$@")"
+  fi
+
+  printf ">>> %s\n" "$msg"
+}
