800a6d18c1b3965bcceb34bc7a25513365e30e09
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-12 16:47:40 -0400

    Add skeleton config for mason.nvim plugin

---
+++ home/dot_config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,6 @@
+--- Portable package manager for Neovim that runs everywhere Neovim runs. Easily install and manage LSP servers, DAP servers, linters, and formatters.
+
+return {
+	-- PLUGIN: http://github.com/williamboman/mason.nvim
+	{ "williamboman/mason.nvim", opts = {}, lazy = false },
+}
