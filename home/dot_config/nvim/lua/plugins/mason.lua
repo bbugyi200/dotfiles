972a9d417181fb2cccdb63e7cf2a505294dd5c2a
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-12 17:31:25 -0400

    Add some lsp servers and formatting / linting tools to mason

--- home/dot_config/nvim/lua/plugins/mason.lua
+++ home/dot_config/nvim/lua/plugins/mason.lua
@@ -1,6 +1,44 @@
---- Portable package manager for Neovim that runs everywhere Neovim runs. Easily install and manage LSP servers, DAP servers, linters, and formatters.
+--- A package manager used to install external editor tools for use within Neovim.
+---
+--- Allows you to easily manage external editor tooling such as LSP servers,
+--- DAP servers, linters, and formatters through a single interface.
 
+local mason_plugin_name = "williamboman/mason.nvim"
 return {
 	-- PLUGIN: http://github.com/williamboman/mason.nvim
-	{ "williamboman/mason.nvim", opts = {}, lazy = false },
+	{
+		mason_plugin_name,
+		opts = {
+			pip = { upgrade_pip = true },
+		},
+		lazy = false,
+	},
+	-- PLUGIN: http://github.com/williamboman/mason-lspconfig.nvim
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = mason_plugin_name,
+		opts = {
+			ensure_installed = {
+				"bashls",
+				"jedi_language_server",
+				"lua_ls",
+				"ruff",
+				"vimls",
+			},
+		},
+	},
+	-- PLUGIN: http://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_installed = {
+				"black",
+				"isort",
+				"rustfmt",
+				"shellcheck",
+				"shfmt",
+				"stylua",
+			},
+		},
+	},
 }
