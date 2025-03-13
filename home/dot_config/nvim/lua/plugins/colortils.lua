114fda17d1af697bac6b1f38b93446bf64000d0a
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-13 18:08:01 -0400

    Add ,ct keymap for :Colortils

--- home/dot_config/nvim/lua/plugins/colortils.lua
+++ home/dot_config/nvim/lua/plugins/colortils.lua
@@ -2,5 +2,17 @@
 
 return {
 	-- PLUGIN: http://github.com/max397574/colortils.nvim
-	{ "max397574/colortils.nvim", opts = {} },
+	{
+		"max397574/colortils.nvim",
+		opts = {},
+		init = function()
+			-- KEYMAP(N): <leader>ct
+			vim.keymap.set(
+				"n",
+				"<leader>ct",
+				"<cmd>Colortils picker <c-r><c-w><cr>",
+				{ desc = "Open colortils picker using color under cursor." }
+			)
+		end,
+	},
 }
