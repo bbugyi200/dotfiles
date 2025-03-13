c46a34df7b8bf671103c677890d86c600df3042f
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-13 12:13:28 -0400

    Add smoke tests for plugins.* modules

--- home/dot_config/nvim/lua/tests/e2e_spec.lua
+++ home/dot_config/nvim/lua/tests/e2e_spec.lua
@@ -7,22 +7,32 @@ local pending = require("busted").pending
 
 --- Scan target path and fetch all top-level file/directory names.
 ---
----@param path string Path to the target directory.
----@return table<string> List of file/directory names.
-local function scandir(path)
-	local entries = {}
+---@param parent_pack_base string Name of the parent directory.
+---@return table<string> # List of all basenames in the parent directory.
+local function get_all_subpacks(parent_pack_base)
+	local cwd = os.getenv("PWD") or ""
+	assert(cwd ~= "", "PWD environment variable is not set")
+	local parent_pack = cwd .. "/home/dot_config/nvim/lua/" .. parent_pack_base
+
+	local all_file_names = {}
 	-- Determine OS-specific command
-	local cmd = package.config:sub(1, 1) == "\\" and ('dir "' .. path .. '" /b') or ('ls -A "' .. path .. '"')
+	local cmd = package.config:sub(1, 1) == "\\" and ('dir "' .. parent_pack .. '" /b')
+		or ('ls -A "' .. parent_pack .. '"')
 	local handle = io.popen(cmd)
 	if handle then
 		for entry in handle:lines() do
-			table.insert(entries, entry)
+			table.insert(all_file_names, entry)
 		end
 		handle:close()
 	else
-		error("Failed to read directory: " .. path)
+		error("Failed to read directory: " .. parent_pack)
 	end
-	return entries
+	local subpacks = {}
+	for _, lua_file in ipairs(all_file_names) do
+		local subpack = lua_file:gsub("%.lua$", "")
+		subpacks[#subpacks + 1] = subpack
+	end
+	return subpacks
 end
 
 -- P4: Migrate to unit tests, since we don't need an embedded Neovim process?
@@ -41,32 +51,32 @@ describe("Smoke test that", function()
 		vim.fn.jobstop(nvim)
 	end)
 
-	local cwd = os.getenv("PWD") or ""
-	assert(cwd ~= "", "PWD environment variable is not set")
-
-	local lua_config_files = scandir(cwd .. "/home/dot_config/nvim/lua/config")
-	-- strip all .lua extensions from entries
-	local config_subpacks = {}
-	for _, lua_file in ipairs(lua_config_files) do
-		local subpack = lua_file:gsub("%.lua$", "")
+	local config_subpacks = get_all_subpacks("config")
+	for _, subpack in ipairs(config_subpacks) do
 		if subpack ~= "lazy_plugins" then
-			config_subpacks[#config_subpacks + 1] = subpack
+			it("sources config." .. subpack, function()
+				local result = vim.fn.rpcrequest(nvim, "nvim_cmd", {
+					cmd = "lua",
+					args = { string.format("vim.print(pcall(require, 'config.%s'))", subpack) },
+					---@diagnostic disable-next-line: redundant-parameter
+				}, { output = true })
+				local ok, ret = string.match(result, "(%S+)%s*(.*)")
+				assert.is_equal(ok, "true", "ERROR IN 'config." .. subpack .. "' MODULE: " .. ret)
+				assert.is_equal(ret, "true")
+			end)
 		end
 	end
 
-	for _, subpack in ipairs(config_subpacks) do
-		it("sources config." .. subpack, function()
+	local plugin_subpacks = get_all_subpacks("plugins")
+	for _, subpack in ipairs(plugin_subpacks) do
+		it("sources plugins." .. subpack, function()
 			local result = vim.fn.rpcrequest(nvim, "nvim_cmd", {
 				cmd = "lua",
-				args = { string.format("vim.print(pcall(require, 'config.%s'))", subpack) },
+				args = { string.format("vim.print(pcall(require, 'plugins.%s'))", subpack) },
 				---@diagnostic disable-next-line: redundant-parameter
 			}, { output = true })
 			local ok, ret = string.match(result, "(%S+)%s*(.*)")
-			assert.is_equal(ok, "true", "ERROR IN 'config." .. subpack .. "' MODULE: " .. ret)
-			assert.is_equal(ret, "true")
+			assert.is_equal(ok, "true", "ERROR IN 'plugins." .. subpack .. "' MODULE: " .. ret)
 		end)
 	end
-
-	-- P4: TEST: sources each module in the plugins.* namespace
-	pending("sources each module in the plugins.* namespace")
 end)
