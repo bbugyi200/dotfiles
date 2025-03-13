f1a95ffacfac204df0099a70768d9595fc4fd3f4
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-13 17:59:33 -0400

    Install key-analyzer.nvim plugin

---
+++ home/dot_config/nvim/lua/plugins/key_analyzer.lua
@@ -0,0 +1,14 @@
+--- Helping you find free keybinds in Neovim.
+
+return {
+	-- PLUGIN: http://github.com/meznaric/key-analyzer.nvim
+	{
+		"meznaric/key-analyzer.nvim",
+		opts = {},
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		init = function()
+			-- KEYMAP(N): <leader>k
+			vim.keymap.set("n", "<leader>k", ":KeyAnalyzer ", { desc = "Shortcut for :KeyAnalyzer." })
+		end,
+	},
+}
