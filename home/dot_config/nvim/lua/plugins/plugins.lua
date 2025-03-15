f44dc85f450179ab5fb4c6765b49b1a823cb0a48
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-15 17:34:33 -0400

    Add syntax highlighting for zathurarc and applescript files

--- home/dot_config/nvim/lua/plugins/plugins.lua
+++ home/dot_config/nvim/lua/plugins/plugins.lua
@@ -18,6 +18,8 @@ return {
 	"godlygeek/tabular",
 	-- PLUGIN: http://github.com/google/vim-searchindex
 	"google/vim-searchindex",
+	-- PLUGIN: http://github.com/mityu/vim-applescript
+	"mityu/vim-applescript",
 	-- PLUGIN: http://github.com/tpope/vim-abolish
 	"tpope/vim-abolish",
 	-- PLUGIN: http://github.com/tpope/vim-fugitive
--- home/dot_config/nvim/lua/plugins/treesitter.lua
+++ home/dot_config/nvim/lua/plugins/treesitter.lua
@@ -38,6 +38,7 @@ return {
 					"vim",
 					"vimdoc",
 					"yaml",
+					"zathurarc",
 				},
 				sync_install = false,
 				incremental_selection = {
