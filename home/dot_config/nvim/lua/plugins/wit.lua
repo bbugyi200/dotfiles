35d2538b3ec59db8b784f8210e2e1afba0260547
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-16 18:14:17 -0400

    Add :WitGitHub command

--- home/dot_config/nvim/lua/plugins/wit.lua
+++ home/dot_config/nvim/lua/plugins/wit.lua
@@ -11,18 +11,24 @@ return {
 				local url = "https://moma.corp.google.com/search?q=" .. opts.args
 				wit.open_url(url)
 			end, { nargs = 1 })
+			vim.api.nvim_create_user_command("WitGitHub", function(opts)
+				local url = "https://github.com/search?type=code&q=" .. opts.args
+				wit.open_url(url)
+			end, { nargs = 1 })
 
 			-- KEYMAP(N): <leader>wg
 			vim.keymap.set(
 				"n",
 				"<leader>wg",
 				":WitSearch site:github.com ",
-				{ desc = "Shortcut for (GitHub) :WitSearch." }
+				{ desc = "Shortcut for (GitHub) :WitSearch" }
 			)
+			-- KEYMAP(N): <leader>wh
+			vim.keymap.set("n", "<leader>wh", ":WitGitHub ", { desc = "Shortcut for :WitGitHub" })
 			-- KEYMAP(N): <leader>wm
-			vim.keymap.set("n", "<leader>wm", ":WitMoma ", { desc = "Shortcut for :WitMoma." })
+			vim.keymap.set("n", "<leader>wm", ":WitMoma ", { desc = "Shortcut for :WitMoma" })
 			-- KEYMAP(N): <leader>ww
-			vim.keymap.set("n", "<leader>ww", ":WitSearch ", { desc = "Shortcut for :WitSearch." })
+			vim.keymap.set("n", "<leader>ww", ":WitSearch ", { desc = "Shortcut for :WitSearch" })
 		end,
 	},
 }
