cffa3447cb4b5f9d320a48a2d0d57ce60e3b3fcf
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-21 09:31:38 -0400

    Add ,wd keymap for Googling word definitions

--- home/dot_config/nvim/lua/plugins/wit.lua
+++ home/dot_config/nvim/lua/plugins/wit.lua
@@ -13,19 +13,21 @@ return {
 				local url = "https://moma.corp.google.com/search?q=" .. opts.args
 				wit.open_url(url)
 			end, { nargs = 1 })
-
 			-- COMMAND: WitGitHub
 			vim.api.nvim_create_user_command("WitGitHub", function(opts)
 				local url = "https://github.com/search?type=code&q=" .. opts.args
 				wit.open_url(url)
 			end, { nargs = 1 })
-
 			-- COMMAND: WitYouTube
 			vim.api.nvim_create_user_command("WitYouTube", function(opts)
 				local url = "https://www.youtube.com/results?search_query=" .. opts.args
 				wit.open_url(url)
 			end, { nargs = 1 })
 
+			-- KEYMAP(N): <leader>wd
+			vim.keymap.set("n", "<leader>wd", ":WitSearch define ", {
+				desc = "Shortcut for ':WitSearch define'",
+			})
 			-- KEYMAP(N): <leader>wg
 			vim.keymap.set(
 				"n",
@@ -33,16 +35,12 @@ return {
 				":WitSearch site:github.com ",
 				{ desc = "Shortcut for ':WitSearch site:github.com'" }
 			)
-
 			-- KEYMAP(N): <leader>wh
 			vim.keymap.set("n", "<leader>wh", ":WitGitHub ", { desc = "Shortcut for :WitGitHub" })
-
 			-- KEYMAP(N): <leader>wm
 			vim.keymap.set("n", "<leader>wm", ":WitMoma ", { desc = "Shortcut for :WitMoma" })
-
 			-- KEYMAP(N): <leader>ww
 			vim.keymap.set("n", "<leader>ww", ":WitSearch ", { desc = "Shortcut for :WitSearch" })
-
 			-- KEYMAP(N): <leader>wy
 			vim.keymap.set("n", "<leader>wy", ":WitYouTube ", { desc = "Shortcut for :WitYouTube" })
 		end,
