f185bb7a7c09d9e69dbbc4d12e225c4c004b6a2f
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-23 08:31:57 -0400

    Configure firenvim to take over only once automatically

--- home/dot_config/nvim/lua/plugins/firenvim.lua
+++ home/dot_config/nvim/lua/plugins/firenvim.lua
@@ -2,5 +2,22 @@
 
 return {
 	-- PLUGIN: http://github.com/glacambre/firenvim
-	{ "glacambre/firenvim", build = ":call firenvim#install(0)" },
+	{
+		"glacambre/firenvim",
+		build = ":call firenvim#install(0)",
+		init = function()
+			vim.g.firenvim_config = {
+				globalSettings = { alt = "all" },
+				localSettings = {
+					[".*"] = {
+						cmdline = "neovim",
+						content = "text",
+						priority = 0,
+						selector = "textarea",
+						takeover = "once",
+					},
+				},
+			}
+		end,
+	},
 }
