e8cd74498805e5f1608aaf8edd0db03d100f6e45
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-23 12:30:29 -0400

    Install indent-blankline.nvim and rainbow-delimiters.nvim plugins

---
+++ home/dot_config/nvim/lua/plugins/rainbow.lua
@@ -0,0 +1,59 @@
+--- Rainbow highlights in Neovim.
+---
+--- indent-blankline.nvim: Indent Guides for Neovim.
+--- rainbow-delimiters.nvim: Rainbow delimiters for Neovim with Tree-sitter.
+
+local highlight = {
+	"RainbowRed",
+	"RainbowYellow",
+	"RainbowBlue",
+	"RainbowOrange",
+	"RainbowGreen",
+	"RainbowViolet",
+	"RainbowCyan",
+}
+
+return {
+	-- PLUGIN: http://github.com/lukas-reineke/indent-blankline.nvim
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		init = function()
+			local hooks = require("ibl.hooks")
+			-- create the highlight groups in the highlight setup hook, so they are reset
+			-- every time the colorscheme changes
+			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+				vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+				vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+				vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+				vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+				vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+				vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+				vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+			end)
+			hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+			require("ibl").setup({ indent = { highlight = highlight } })
+		end,
+	},
+	-- PLUGIN: http://github.com/HiPhish/rainbow-delimiters.nvim
+	{
+		"HiPhish/rainbow-delimiters.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		main = "rainbow-delimiters.setup",
+		opts = {
+			strategy = {
+				[""] = "rainbow-delimiters.strategy.global",
+				vim = "rainbow.delimiters.strategy.local",
+			},
+			query = {
+				[""] = "rainbow-delimiters",
+				lua = "rainbow-blocks",
+			},
+			priority = {
+				[""] = 110,
+				lua = 210,
+			},
+			highlight = highlight,
+		},
+	},
+}
