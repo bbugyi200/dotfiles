df26ed130898640a550be7babb784c1103226b90
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-24 19:25:38 -0400

    Delete unused Trouble ,x* keymaps

--- home/dot_config/nvim/lua/plugins/trouble.lua
+++ home/dot_config/nvim/lua/plugins/trouble.lua
@@ -9,24 +9,5 @@ return {
 			"nvim-tree/nvim-web-devicons",
 		},
 		opts = { focus = true, win = { size = 0.3 } },
-		init = function()
-			-- Mappings
-			--
-			-- P4: Add KEYMAP comments + descriptions (Desc)!
-			vim.api.nvim_set_keymap("n", "<Leader>xw", "<cmd>Trouble<cr>", { silent = true, noremap = true })
-			vim.api.nvim_set_keymap(
-				"n",
-				"<Leader>xd",
-				"<cmd>Trouble filter.buf=0<cr>",
-				{ silent = true, noremap = true }
-			)
-			vim.api.nvim_set_keymap("n", "<Leader>xl", "<cmd>Trouble loclist<cr>", { silent = true, noremap = true })
-			vim.api.nvim_set_keymap(
-				"n",
-				"<Leader>xq",
-				"<cmd>cclose<cr><cmd>Trouble quickfix<cr>",
-				{ silent = true, noremap = true }
-			)
-		end,
 	},
 }
