b56c1b487a0bb694b0effd7d8e1be099c92f84e2
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-25 13:14:34 -0400

    Add ,d>; ,d<; and ,do keymaps

--- home/dot_config/nvim/lua/config/keymaps/delete_buffers.lua
+++ home/dot_config/nvim/lua/config/keymaps/delete_buffers.lua
@@ -3,6 +3,36 @@
 local kill_buffer = require("util.kill_buffer").kill_buffer
 local delete_file = require("util.delete_file")
 
+--- Deletes all buffers to the left of the current buffer.
+local function delete_buffers_to_left()
+	local current_buf = vim.fn.bufnr("%")
+	for bufnum = 1, current_buf - 1 do
+		if vim.fn.bufexists(bufnum) == 1 and vim.fn.buflisted(bufnum) == 1 then
+			vim.cmd("bdelete " .. bufnum)
+		end
+	end
+end
+
+--- Deletes all buffers to the right of the current buffer.
+local function delete_buffers_to_right()
+	local current_buf = vim.fn.bufnr("%")
+	for bufnum = current_buf + 1, vim.fn.bufnr("$") do
+		if vim.fn.bufexists(bufnum) == 1 and vim.fn.buflisted(bufnum) == 1 then
+			vim.cmd("bdelete " .. bufnum)
+		end
+	end
+end
+
+-- KEYMAP(N): <leader>d<
+vim.keymap.set("n", "<leader>d<", delete_buffers_to_left, {
+	desc = "Delete all buffers to the left of the current buffer.",
+})
+
+-- KEYMAP(N): <leader>d>
+vim.keymap.set("n", "<leader>d>", delete_buffers_to_right, {
+	desc = "Delete all buffers to the right of the current buffer.",
+})
+
 -- KEYMAP(N): <leader>dd
 vim.keymap.set("n", "<leader>dd", function()
 	kill_buffer("#")
@@ -12,6 +42,11 @@ end, { desc = "Remove the current buffer and navigate back to the last active bu
 vim.keymap.set("n", "<leader>dn", function()
 	kill_buffer("next")
 end, { desc = "Remove the current buffer and navigate to the next buffer." })
+
+-- KEYMAP(N): <leader>do
+vim.keymap.set("n", "<leader>do", "<cmd>%bd|e#<cr>", {
+	desc = "Delete all buffers but the current buffer.",
+})
 
 -- KEYMAP(N): <leader>dp
 vim.keymap.set("n", "<leader>dp", function()
