03dccb9c47c45c3aa9492dd23d6b9b854eae842d
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-26 18:34:59 -0400

    Install nvim-lightbulb plugin

---
+++ home/dot_config/nvim/lua/plugins/lightbulb.lua
@@ -0,0 +1,9 @@
+--- VSCode 💡 for neovim's built-in LSP.
+
+return {
+	-- PLUGIN: http://github.com/kosayoda/nvim-lightbulb
+	{
+		"kosayoda/nvim-lightbulb",
+		opts = { autocmd = { enabled = true } },
+	},
+}
