44d5ef5508f1c0815646600e76808527caeb1a95
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-26 20:20:31 -0400

    Install (unconfigured) octo.nvim plugin

---
+++ home/dot_config/nvim/lua/plugins/octo.lua
@@ -0,0 +1,9 @@
+--- Edit and review GitHub issues and pull requests from the comfort of your favorite editor.
+
+return {
+	-- PLUGIN: http://github.com/pwntester/octo.nvim
+	{
+		"pwntester/octo.nvim",
+		opts = {},
+	},
+}
