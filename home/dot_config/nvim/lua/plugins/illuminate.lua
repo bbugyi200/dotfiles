86e939a7178b7a79903bf10fd08da36c16c62710
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-27 18:08:54 -0400

    Attempt to fix illuminate.nvim highlight colors

---
+++ home/dot_config/nvim/lua/plugins/illuminate.lua
@@ -0,0 +1,16 @@
+--- (Neo)Vim plugin for automatically highlighting other uses of the word under
+--- the cursor using either LSP, Tree-sitter, or regex matching.
+
+-- PLUGIN: http://github.com/RRethy/vim-illuminate
+return {
+	{
+		"RRethy/vim-illuminate",
+		init = function()
+			vim.cmd([[
+    hi IlluminatedWordText guifg=none guibg=none gui=standout,bold
+    hi IlluminatedWordRead guifg=none guibg=none gui=standout,bold
+    hi IlluminatedWordWrite guifg=none guibg=none gui=standout,bold
+    ]])
+		end,
+	},
+}
--- home/dot_config/nvim/lua/plugins/plugins.lua
+++ home/dot_config/nvim/lua/plugins/plugins.lua
@@ -22,8 +22,6 @@ return {
 	"jamessan/vim-gnupg",
 	-- PLUGIN: http://github.com/mityu/vim-applescript
 	"mityu/vim-applescript",
-	-- PLUGIN: http://github.com/RRethy/vim-illuminate
-	"RRethy/vim-illuminate",
 	-- PLUGIN: http://github.com/wellle/targets.vim
 	"wellle/targets.vim",
 }
