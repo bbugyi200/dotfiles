b946ca8071454fa0e84b7c7e2c2dea3bf94d3296
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-03-27 20:21:44 -0400

    Install markview.nvim plugin

---
+++ home/dot_config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,9 @@
+--- A hackable markdown, Typst, latex, html(inline) & YAML previewer for Neovim.
+
+return {
+	-- PLUGIN: http://github.com/OXY2DEV/markview.nvim
+	{
+		"OXY2DEV/markview.nvim",
+		lazy = false,
+	},
+}
