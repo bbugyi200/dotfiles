e05548931fdc8cc1dc7b38dad43a27a6fb87a73a
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-04-08 12:55:32 -0400

    feat: Install nvim-silicon plugin

---
+++ home/dot_config/nvim/lua/plugins/silicon.lua
@@ -0,0 +1,12 @@
+--- neovim plugin to create code images using the external silicon tool.
+
+return {
+	-- PLUGIN: http://github.com/michaelrommel/nvim-silicon
+	{
+		"michaelrommel/nvim-silicon",
+		lazy = true,
+		cmd = "Silicon",
+		main = "nvim-silicon",
+		opts = {},
+	},
+}
