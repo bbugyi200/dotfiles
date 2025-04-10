ef63cf09159bce6395861511858ca06a133fa905
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-04-10 13:24:51 -0400

    fix: Only install avante.nvim on non-google machines

--- home/dot_config/nvim/lua/plugins/avante.lua
+++ home/dot_config/nvim/lua/plugins/avante.lua
@@ -1,18 +1,26 @@
 --- Use your Neovim like using Cursor AI IDE!
 
-return {
-	-- PLUGIN: http://github.com/yetone/avante.nvim
-	{
-		"yetone/avante.nvim",
-		build = "make",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"stevearc/dressing.nvim",
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
+local is_goog_machine = require("util.is_goog_machine")
+
+if is_goog_machine() then
+	-- When working from a Google machine, I am not allowed to use external LLM
+	-- tools like OpenAI or Anthropic.
+	return {}
+else
+	return {
+		-- PLUGIN: http://github.com/yetone/avante.nvim
+		{
+			"yetone/avante.nvim",
+			build = "make",
+			dependencies = {
+				"nvim-treesitter/nvim-treesitter",
+				"stevearc/dressing.nvim",
+				"nvim-lua/plenary.nvim",
+				"MunifTanjim/nui.nvim",
+			},
+			event = "VeryLazy",
+			version = false,
+			opts = {},
 		},
-		event = "VeryLazy",
-		version = false,
-		opts = {},
-	},
-}
+	}
+end
