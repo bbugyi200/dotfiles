8568455abdc1dab81d7b57894329f9ff330c585e
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-04-14 12:52:12 -0400

    fix: Remove lightbulb plugin (redundant with lspsaga)

--- home/dot_config/nvim/lua/plugins/lightbulb.lua
+++
@@ -1,9 +0,0 @@
---- VSCode 💡 for neovim's built-in LSP.
-
-return {
-	-- PLUGIN: http://github.com/kosayoda/nvim-lightbulb
-	{
-		"kosayoda/nvim-lightbulb",
-		opts = { autocmd = { enabled = true } },
-	},
-}
