1ca424051c6d1993cc7f8da754423924f18547b3
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-04-15 13:12:39 -0400

    feat: Uncomment diffview plugin (I don't _think_ this was the cause)

--- home/dot_config/nvim/lua/plugins/diffview.lua
+++ home/dot_config/nvim/lua/plugins/diffview.lua
@@ -1,9 +1,9 @@
 --- Single tabpage interface for easily cycling through diffs for all modified files for any git rev.
 
 return {
-	-- -- PLUGIN: http://github.com/sindrets/diffview.nvim
-	-- {
-	-- 	"sindrets/diffview.nvim",
-	-- 	opts = {},
-	-- },
+	-- PLUGIN: http://github.com/sindrets/diffview.nvim
+	{
+		"sindrets/diffview.nvim",
+		opts = {},
+	},
 }
