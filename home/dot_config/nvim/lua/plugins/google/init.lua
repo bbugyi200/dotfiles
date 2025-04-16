40e942434d9373b3c691d67aabc64375fd75c1f2
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-04-16 11:47:36 -0400

    doc: Remove all google/init.lua todo comments

--- home/dot_config/nvim/lua/plugins/google/init.lua
+++ home/dot_config/nvim/lua/plugins/google/init.lua
@@ -1,10 +1,3 @@
--- P1: Use vim.tbl_extend() instead of merge_tables()!
--- P2: Install ALL plugins listed in go/neovim!
--- P2: Flex critique plugins!
--- P2: Configure http://go/analysislsp-neovim !
--- P2: Add neocitc integrations described by
---     https://team.git.corp.google.com/neovim-dev/neocitc/+/refs/heads/main
-
 local is_goog_machine = require("util.is_goog_machine")
 
 if is_goog_machine() then
