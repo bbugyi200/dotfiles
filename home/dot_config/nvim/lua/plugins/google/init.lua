af51d3fc73065f40639c0afe929b1c211726ca5e
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-04-16 11:44:00 -0400

    ref: Use vim.list_extend() instead of merge_tables()

--- home/dot_config/nvim/lua/plugins/google/init.lua
+++ home/dot_config/nvim/lua/plugins/google/init.lua
@@ -9,19 +9,9 @@
 
 local is_goog_machine = require("util.is_goog_machine")
 
---- Performs an in-place merge of two array-like Lua tables.
----
----@generic T : table
----@param t1 T The first table to merge, which will be modified in-place.
----@param t2 T The second table to merge.
----@return T # The 1st table (t1) with the contents of the 2nd table (t2) merged in.
-local function merge_tables(t1, t2)
-	table.move(t2, 1, #t2, #t1 + 1, t1)
-	return t1
-end
-
 if is_goog_machine() then
-	return merge_tables(require("plugins.google.glugs"), require("plugins.google.non_glugs"))
+	-- Use vim.list_extend to merge the two arrays of plugins
+	return vim.list_extend(require("plugins.google.glugs"), require("plugins.google.non_glugs"))
 else
 	return {}
 end
