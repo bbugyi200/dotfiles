d0015c23d3414bce7e20785fbf6f0637a7f1964d
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-04-18 14:12:45 -0400

    ref: Factor out get_shortcuts() function

--- home/dot_config/nvim/lua/plugins/dashboard.lua
+++ home/dot_config/nvim/lua/plugins/dashboard.lua
@@ -2,6 +2,60 @@
 
 local is_goog_machine = require("util.is_goog_machine")
 
+local function get_shortcuts()
+	local shortcuts = {}
+
+	-- Only add CodeSearch shortcut if on a Google machine
+	if is_goog_machine() then
+		table.insert(shortcuts, {
+			desc = "🔭 CodeSearch",
+			group = "Special",
+			action = "Telescope codesearch find_query",
+			key = "c",
+		})
+	end
+
+	-- Add the rest of the shortcuts
+	table.insert(shortcuts, {
+		desc = " dotfiles",
+		group = "Number",
+		action = "Telescope chezmoi find_files",
+		key = "d",
+	})
+
+	table.insert(shortcuts, {
+		icon = " ",
+		icon_hl = "@variable",
+		desc = "Files",
+		group = "Label",
+		action = "Telescope find_files",
+		key = "f",
+	})
+
+	table.insert(shortcuts, {
+		desc = " Sessions",
+		group = "@function",
+		action = "Autosession search",
+		key = "s",
+	})
+
+	table.insert(shortcuts, {
+		desc = "󰊳 Update",
+		group = "@property",
+		action = "Lazy update",
+		key = "u",
+	})
+
+	table.insert(shortcuts, {
+		desc = "❌ Quit",
+		group = "ErrorMsg",
+		action = "quit",
+		key = "q",
+	})
+
+	return shortcuts
+end
+
 return {
 	-- PLUGIN: http://github.com/nvimdev/dashboard-nvim
 	{
@@ -9,63 +63,13 @@ return {
 		event = "VimEnter",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
-			local shortcuts = {}
-
-			-- Only add CodeSearch shortcut if on a Google machine
-			if is_goog_machine() then
-				table.insert(shortcuts, {
-					desc = "🔭 CodeSearch",
-					group = "Special",
-					action = "Telescope codesearch find_query",
-					key = "c",
-				})
-			end
-
-			-- Add the rest of the shortcuts
-			table.insert(shortcuts, {
-				desc = " dotfiles",
-				group = "Number",
-				action = "Telescope chezmoi find_files",
-				key = "d",
-			})
-
-			table.insert(shortcuts, {
-				icon = " ",
-				icon_hl = "@variable",
-				desc = "Files",
-				group = "Label",
-				action = "Telescope find_files",
-				key = "f",
-			})
-
-			table.insert(shortcuts, {
-				desc = " Sessions",
-				group = "@function",
-				action = "Autosession search",
-				key = "s",
-			})
-
-			table.insert(shortcuts, {
-				desc = "󰊳 Update",
-				group = "@property",
-				action = "Lazy update",
-				key = "u",
-			})
-
-			table.insert(shortcuts, {
-				desc = "❌ Quit",
-				group = "ErrorMsg",
-				action = "quit",
-				key = "q",
-			})
-
 			require("dashboard").setup({
 				theme = "hyper",
 				config = {
 					week_header = {
 						enable = true,
 					},
-					shortcut = shortcuts,
+					shortcut = get_shortcuts(),
 				},
 			})
 		end,
