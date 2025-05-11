28cedd48027d03ef186c1f1dd726484cbce127ab
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-05-11 12:15:53 -0400

    fix: Only install codecompanion.nvim on non-goog machines

--- home/dot_config/nvim/lua/plugins/codecompanion.lua
+++ home/dot_config/nvim/lua/plugins/codecompanion.lua
@@ -1,14 +1,22 @@
 --- AI-powered coding, seamlessly in Neovim.
 
-return {
-	-- PLUGIN: http://github.com/olimorris/codecompanion.nvim
-	{
-		"olimorris/codecompanion.nvim",
-		version = "*",
-		dependencies = {
-			{ "nvim-lua/plenary.nvim", branch = "master" },
-			"nvim-treesitter/nvim-treesitter",
+local bb = require("bb_utils")
+
+if bb.is_goog_machine() then
+	-- When working from a Google machine, I am not allowed to use external LLM
+	-- tools like OpenAI or Anthropic.
+	return {}
+else
+	return {
+		-- PLUGIN: http://github.com/olimorris/codecompanion.nvim
+		{
+			"olimorris/codecompanion.nvim",
+			version = "*",
+			dependencies = {
+				{ "nvim-lua/plenary.nvim", branch = "master" },
+				"nvim-treesitter/nvim-treesitter",
+			},
+			opts = {},
 		},
-		opts = {},
-	},
-}
+	}
+end
