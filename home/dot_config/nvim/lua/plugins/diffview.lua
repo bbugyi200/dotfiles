fc52e53acc1c3c03238d53186a7a51ad29be5bc1
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-05-13 09:41:02 -0400

    feat: Add 'q' keymap to quit diffview

--- home/dot_config/nvim/lua/plugins/diffview.lua
+++ home/dot_config/nvim/lua/plugins/diffview.lua
@@ -4,7 +4,17 @@ return {
 	-- PLUGIN: http://github.com/sindrets/diffview.nvim
 	{
 		"sindrets/diffview.nvim",
-		opts = {},
+		opts = {
+			hooks = {
+				view_opened = function(_)
+					-- KEYMAP: q
+					vim.keymap.set("n", "q", "<cmd>tabclose<cr>", { desc = "Close diffview." })
+				end,
+				view_closed = function(_)
+					vim.keymap.del("n", "q")
+				end,
+			},
+		},
 		init = function()
 			-- KEYMAP: <leader>dv
 			vim.keymap.set("n", "<leader>dv", "<cmd>DiffviewOpen<cr>", { desc = "DiffviewOpen" })
