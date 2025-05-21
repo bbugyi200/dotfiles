2f47f1527d1c912e3ae1bdcb3f89e1a24051d62d
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-05-21 12:51:13 -0400

    feat: Add @e and #b codecompanion snippets

---
+++ home/dot_config/nvim/luasnippets/codecompanion.lua
@@ -0,0 +1,8 @@
+-- Snippets for CodeCompanion chat buffer.
+
+return {
+	-- SNIPPET: @e
+	s({ trig = "@e", desc = "Auto-snippet for @editor", snippetType = "autosnippet" }, { t("@editor") }),
+	-- SNIPPET: #b
+	s({ trig = "#b", desc = "Auto-snippet for #buffer", snippetType = "autosnippet" }, { t("#buffer") }),
+}
