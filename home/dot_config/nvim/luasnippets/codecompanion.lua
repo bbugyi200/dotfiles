2751f09eae377bc2a029ef3b65604a6829f09152
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-05-22 10:43:56 -0400

    feat: Make all cc snippets hidden

--- home/dot_config/nvim/luasnippets/codecompanion.lua
+++ home/dot_config/nvim/luasnippets/codecompanion.lua
@@ -2,12 +2,18 @@
 
 return {
 	-- SNIPPET: @e
-	s({ trig = "@e", desc = "Auto-snippet for @editor", snippetType = "autosnippet" }, { t("@editor ") }),
+	s(
+		{ trig = "@e", desc = "Auto-snippet for @editor", snippetType = "autosnippet", hidden = true },
+		{ t("@editor ") }
+	),
 	-- SNIPPET: @u
 	s(
-		{ trig = "@u", desc = "Use @editor on #buffer to...", snippetType = "autosnippet" },
+		{ trig = "@u", desc = "Use @editor on #buffer to...", snippetType = "autosnippet", hidden = true },
 		{ t("Use @editor on #buffer to ") }
 	),
 	-- SNIPPET: #b
-	s({ trig = "#b", desc = "Auto-snippet for #buffer", snippetType = "autosnippet" }, { t("#buffer ") }),
+	s(
+		{ trig = "#b", desc = "Auto-snippet for #buffer", snippetType = "autosnippet", hidden = true },
+		{ t("#buffer ") }
+	),
 }
