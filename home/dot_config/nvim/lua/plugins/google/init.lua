cfd39b83013e9f4b1850e89c3c8e29853d9edf3b
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-05-24 19:05:44 -0400

    fix: another attempt to fix google/foodir imports

--- home/dot_config/nvim/lua/plugins/google/init.lua
+++ home/dot_config/nvim/lua/plugins/google/init.lua
@@ -12,7 +12,7 @@ this_dir = this_dir or vim.fn.expand("%:p:h") .. "/"
 local M = {}
 
 -- Find all Lua files in the current directory
-local files = vim.fn.glob(this_dir .. "*.lua", false, true)
+local files = vim.fn.glob(this_dir .. "*", false, true)
 
 for _, file in ipairs(files) do
 	local filename = vim.fn.fnamemodify(file, ":t")
@@ -28,21 +28,5 @@ for _, file in ipairs(files) do
 		end
 	end
 end
--- Find all directories in the current directory
-local dirs = vim.fn.glob(this_dir .. "/*", false, true)
 
-for _, dir in ipairs(dirs) do
-	-- Get the directory name without the trailing slash
-	local dirname = vim.fn.fnamemodify(dir, ":h:t")
-
-	-- Check if the directory has an init.lua file
-	local init_file = dir .. "init.lua"
-	if vim.fn.filereadable(init_file) == 1 then
-		local ok, mod = pcall(require, "plugins.google." .. dirname)
-
-		if ok and type(mod) == "table" then
-			vim.list_extend(M, mod)
-		end
-	end
-end
 return M
