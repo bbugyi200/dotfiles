58f42a05c9c17acf169e7125a7a3829026dc93d8
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-05-27 08:28:22 -0400

    lint: Fix line-too-long linting errors

--- home/dot_config/nvim/lua/plugins/google/codecompanion/init.lua
+++ home/dot_config/nvim/lua/plugins/google/codecompanion/init.lua
@@ -27,14 +27,14 @@ return {
 			goose.setup({
 				-- Add your options here
 				-- These are the defaults
-				auto_start_backend = false, -- Whether to automatically start go/devai-api-http-proxy. If false you can use :AvanteGooseServerStart to start the server
-				auto_start_silent = false, -- Whether to have a silent auto start (don't log status messages)
-				model = "goose-v3.5-m", -- Select model from go/goose-models.
-				temperature = 0.1, -- Model temperature
-				max_decoder_tokens = 8192, -- Max decoder tokens
-				endpoint = "http://localhost:8649/predict", -- Endpoint to start/listen to go/devai-api-http-proxy
-				debug = true, -- Enables debug mode (outputs lots of logs for troubleshooting issues)
-				debug_backend = false, -- Whether to start the backend in debug mode. This logs backend output information under stdpath('cache')/devai-http-wrapper.log
+				auto_start_backend = false,
+				auto_start_silent = false,
+				model = "goose-v3.5-m",
+				temperature = 0.1,
+				max_decoder_tokens = 8192,
+				endpoint = "http://localhost:8649/predict",
+				debug = true,
+				debug_backend = false,
 			})
 
 			require("codecompanion").setup({
