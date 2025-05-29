ee164c83107aff72ac2cf039426ac90421bf8e70
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-05-29 11:24:07 -0400

    fix: Switch back to v3.5-s as default model

--- home/dot_config/nvim/lua/plugins/google/codecompanion/init.lua
+++ home/dot_config/nvim/lua/plugins/google/codecompanion/init.lua
@@ -27,7 +27,7 @@ return {
 			goose.setup({
 				auto_start_backend = false,
 				auto_start_silent = false,
-				model = "goose-v3.5-m",
+				model = "goose-v3.5-s",
 				temperature = 0.1,
 				max_decoder_steps = 8192,
 				endpoint = "http://localhost:8649/predict",
