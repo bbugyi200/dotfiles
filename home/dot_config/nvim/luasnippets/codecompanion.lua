47e22f585b8e16c29f188ff6f8fdcea3fc95b1c8
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-06-02 16:31:41 -0400

    feat: Use #buffer{watch} for cc snippets

--- home/dot_config/nvim/luasnippets/codecompanion.lua
+++ home/dot_config/nvim/luasnippets/codecompanion.lua
@@ -2,12 +2,24 @@
 
 return {
 	-- SNIPPET: @e
-	s({ trig = "@e", desc = "Auto-snippet for @editor", snippetType = "autosnippet", hidden = true }, { t("@editor") }),
+	s({
+		trig = "@e",
+		desc = "Auto-snippet for @editor",
+		snippetType = "autosnippet",
+		hidden = true,
+	}, { t("@editor") }),
 	-- SNIPPET: @u
-	s(
-		{ trig = "@u", desc = "Use @editor on #buffer to...", snippetType = "autosnippet", hidden = true },
-		{ t("Use @editor on #buffer to ") }
-	),
+	s({
+		trig = "@u",
+		desc = "Use @editor on #buffer{watch} to...",
+		snippetType = "autosnippet",
+		hidden = true,
+	}, { t("Use @editor on #buffer{watch} to ") }),
 	-- SNIPPET: #b
-	s({ trig = "#b", desc = "Auto-snippet for #buffer", snippetType = "autosnippet", hidden = true }, { t("#buffer") }),
+	s({
+		trig = "#b",
+		desc = "Auto-snippet for #buffer{watch}...",
+		snippetType = "autosnippet",
+		hidden = true,
+	}, { t("#buffer{watch}") }),
 }
