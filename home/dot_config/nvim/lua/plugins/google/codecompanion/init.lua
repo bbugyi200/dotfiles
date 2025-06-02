5b7d7e02e8852757d21ef71f9f9fc20303fef2d6
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-06-02 19:06:24 -0400

    feat: Remove version constraint from goog cc plugin

--- home/dot_config/nvim/lua/plugins/google/codecompanion/init.lua
+++ home/dot_config/nvim/lua/plugins/google/codecompanion/init.lua
@@ -2,7 +2,6 @@ return {
 	-- PLUGIN: http://github.com/olimorris/codecompanion.nvim
 	{
 		"olimorris/codecompanion.nvim",
-		version = "*",
 		dependencies = {
 			{ "nvim-lua/plenary.nvim", branch = "master" },
 			"nvim-treesitter/nvim-treesitter",
