d5a411dce6bd162f4c3c4d75686946883946c4f6
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-06-02 16:25:54 -0400

    feat: Enable relativenumber, number, and cursorline for lspsaga buffers

--- home/dot_config/nvim/after/ftplugin/sagafinder.lua
+++ home/dot_config/nvim/after/ftplugin/sagafinder.lua
@@ -1,3 +1,5 @@
 --- Filetype: sagafinder
 
 vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.cursorline = true
--- home/dot_config/nvim/after/ftplugin/sagaoutline.lua
+++ home/dot_config/nvim/after/ftplugin/sagaoutline.lua
@@ -1,3 +1,5 @@
 --- Filetype: sagaoutline
 
 vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.cursorline = true
