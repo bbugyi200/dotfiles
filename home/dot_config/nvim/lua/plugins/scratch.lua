ad6c8f7bc6cee706d060093f6e6b22ad44f9fc42
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-06-05 15:00:26 -0400

    feat: Install (unconfigured) scratch.nvim plugin

---
+++ home/dot_config/nvim/lua/plugins/scratch.lua
@@ -0,0 +1,9 @@
+--- Create temporary playground files effortlessly. Find them later without worrying about filenames or locations.
+
+return {
+	-- PLUGIN: http://github.com/LintaoAmons/scratch.nvim
+	{
+		"LintaoAmons/scratch.nvim",
+		opts = {},
+	},
+}
