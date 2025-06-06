239e271a343f5810e9a4eb142f6d5139237d3bff
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-06-06 12:48:52 -0400

    feat: Configure scratch.nvim

--- home/dot_config/nvim/lua/plugins/scratch.lua
+++ home/dot_config/nvim/lua/plugins/scratch.lua
@@ -4,6 +4,38 @@ return {
 	-- PLUGIN: http://github.com/LintaoAmons/scratch.nvim
 	{
 		"LintaoAmons/scratch.nvim",
-		opts = {},
+		dependencies = {
+			{
+				"ibhagwan/fzf-lua",
+				dependencies = { "nvim-tree/nvim-web-devicons" },
+				opts = {},
+			},
+		},
+		opts = {
+			filetypes = { "dart", "java", "md", "py", "sh", "txt", "zo" },
+			scratch_file_dir = vim.env.HOME .. "/tmp/scratch",
+		},
+		init = function()
+			-- KEYMAP GROUP: <leader>f
+			vim.keymap.set("n", "<leader>f", "<nop>", { desc = "scratch.nvim" })
+			-- KEYMAP: <leader>ff
+			vim.keymap.set("n", "<leader>ff", "<cmd>Scratch<cr>", { desc = "Open a new unnamed scratch file." })
+			-- KEYMAP: <leader>fF
+			vim.keymap.set("n", "<leader>fF", "<cmd>ScratchWithName<cr>", { desc = "Open a new named scratch file." })
+			-- KEYMAP: <leader>fo
+			vim.keymap.set(
+				"n",
+				"<leader>fo",
+				"<cmd>ScratchOpen<cr>",
+				{ desc = "Search filenames for an existing scratch file." }
+			)
+			-- KEYMAP: <leader>fO
+			vim.keymap.set(
+				"n",
+				"<leader>fO",
+				"<cmd>ScratchOpenFzf<cr>",
+				{ desc = "Search file contents for an existing scratch file." }
+			)
+		end,
 	},
 }
