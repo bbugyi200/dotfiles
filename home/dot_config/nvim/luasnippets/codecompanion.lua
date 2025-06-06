6808433ff50983cb44d2af20984dd30647771297
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-06-06 13:09:21 -0400

    feat: Add @@u and @@U cc snippets

--- home/dot_config/nvim/luasnippets/codecompanion.lua
+++ home/dot_config/nvim/luasnippets/codecompanion.lua
@@ -1,4 +1,5 @@
 -- Snippets for CodeCompanion chat buffer.
+local bb = require("bb_utils")
 
 return {
 	-- SNIPPET: @e
@@ -15,6 +16,17 @@ return {
 		snippetType = "autosnippet",
 		hidden = true,
 	}, { t("Use @editor on ") }),
+	-- SNIPPET: @@u
+	s({
+		trig = "@@u",
+		desc = "Use @editor on...",
+		snippetType = "autosnippet",
+		hidden = true,
+	}, {
+		t("Use @editor on "),
+		i(1),
+		t(". Output calls to the editor() function provided by the editor tool."),
+	}),
 	-- SNIPPET: @U
 	s({
 		trig = "@U",
@@ -22,6 +34,17 @@ return {
 		snippetType = "autosnippet",
 		hidden = true,
 	}, { t("Use @editor on #buffer{watch} to ") }),
+	-- SNIPPET: @@U
+	s({
+		trig = "@@U",
+		desc = "Use @editor on #buffer{watch} to...",
+		snippetType = "autosnippet",
+		hidden = true,
+	}, {
+		t("Use @editor on #buffer{watch} to "),
+		i(1),
+		t(". Output calls to the editor() function provided by the editor tool."),
+	}),
 	-- SNIPPET: #b
 	s({
 		trig = "#b",
