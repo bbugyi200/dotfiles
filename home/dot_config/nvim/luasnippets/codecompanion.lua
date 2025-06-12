d98fdc2e5a9f0cc9c1a33ffa880116f8f68fec58
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-06-11 20:04:01 -0400

    Revert "fix: Replace @editor with @insert_edit_into_file"
    This reverts commit 815f2e2f027f49cfe82e74fd650b69a3d95c08ea.

--- home/dot_config/nvim/luasnippets/codecompanion.lua
+++ home/dot_config/nvim/luasnippets/codecompanion.lua
@@ -4,46 +4,46 @@ return {
 	-- SNIPPET: @e
 	s({
 		trig = "@e",
-		desc = "Auto-snippet for @insert_edit_into_file",
+		desc = "Auto-snippet for @editor",
 		snippetType = "autosnippet",
 		hidden = true,
-	}, { t("@insert_edit_into_file") }),
+	}, { t("@editor") }),
 	-- SNIPPET: @@u
 	s({
 		trig = "@@u",
-		desc = "Use @insert_edit_into_file on...",
+		desc = "Use @editor on...",
 		snippetType = "autosnippet",
 		hidden = true,
 	}, {
-		t("Use @insert_edit_into_file on "),
+		t("Use @editor on "),
 		i(1),
 		t(". Output calls to the editor() function provided by the editor tool."),
 	}),
 	-- SNIPPET: @u
 	s({
 		trig = "@u",
-		desc = "Use @insert_edit_into_file on...",
+		desc = "Use @editor on...",
 		snippetType = "autosnippet",
 		hidden = true,
-	}, { t("Use @insert_edit_into_file on ") }),
+	}, { t("Use @editor on ") }),
 	-- SNIPPET: @@U
 	s({
 		trig = "@@U",
-		desc = "Use @insert_edit_into_file on #buffer{watch} to...",
+		desc = "Use @editor on #buffer{watch} to...",
 		snippetType = "autosnippet",
 		hidden = true,
 	}, {
-		t("Use @insert_edit_into_file on #buffer{watch} to "),
+		t("Use @editor on #buffer{watch} to "),
 		i(1),
 		t(". Output calls to the editor() function provided by the editor tool."),
 	}),
 	-- SNIPPET: @U
 	s({
 		trig = "@U",
-		desc = "Use @insert_edit_into_file on #buffer{watch} to...",
+		desc = "Use @editor on #buffer{watch} to...",
 		snippetType = "autosnippet",
 		hidden = true,
-	}, { t("Use @insert_edit_into_file on #buffer{watch} to ") }),
+	}, { t("Use @editor on #buffer{watch} to ") }),
 	-- SNIPPET: #b
 	s({
 		trig = "#b",
