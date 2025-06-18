fc1de87a9c84067a6e070cf105c774e3074127ce
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-06-18 08:53:07 -0400

    ref: Move slash_cmds args around

--- home/dot_config/nvim/lua/plugins/google/codecompanion/slash_cmds/init.lua
+++ home/dot_config/nvim/lua/plugins/google/codecompanion/slash_cmds/init.lua
@@ -1,7 +1,7 @@
 --- Custom Code Companion slash commands live in this module.
 
 return {
+	bugs = require("plugins.google.codecompanion.slash_cmds.bugs"),
 	clfiles = require("plugins.google.codecompanion.slash_cmds.clfiles"),
 	cs = require("plugins.google.codecompanion.slash_cmds.cs"),
-	bugs = require("plugins.google.codecompanion.slash_cmds.bugs"),
 }
