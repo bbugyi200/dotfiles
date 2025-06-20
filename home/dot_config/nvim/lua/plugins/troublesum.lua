f29bcb3ae92a8e2becff632c1cab88a01d1e86cf
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-06-20 09:26:43 -0400

    feat: Install troublesum.nvim plugin

---
+++ home/dot_config/nvim/lua/plugins/troublesum.lua
@@ -0,0 +1,10 @@
+--- Neovim plugin to show diagnostics summary in the buffer's corner, similar
+--- to JetBrains' inspections widget.
+
+return {
+	-- PLUGIN: http://github.com/ivanjermakov/troublesum.nvim
+	{
+		"ivanjermakov/troublesum.nvim",
+		opts = {},
+	},
+}
