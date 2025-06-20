c44b80bd302313815cd02a9c3bcc74912a2a626d
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-06-20 11:02:58 -0400

    feat: Partial migration of 'ge' keymap

--- home/dot_config/nvim/lua/plugins/google/codecompanion/init.lua
+++ home/dot_config/nvim/lua/plugins/google/codecompanion/init.lua
@@ -104,37 +104,17 @@ class Foobar:
 			-- KEYMAP: <leader>ccs
 			cc.create_adapter_switch_keymap("little_goose", "big_goose")
 
-			-- AUTOCMD: Configure 'ge' keymap to comment-paste clipboard and transform code.
+			-- AUTOCMD: Configure 'ge' keymap to quickly implement clipboard edits.
 			vim.api.nvim_create_autocmd("FileType", {
 				pattern = { "codecompanion" },
 				callback = function()
 					-- KEYMAP: ge
 					vim.keymap.set("n", "ge", function()
-						if vim.v.count > 0 then
-							vim.cmd("normal! y" .. vim.v.count .. "j")
-							vim.cmd("normal! " .. vim.v.count + 1 .. "jzz")
-						end
-
-						-- Navigate to previous buffer
+						vim.fn.search("\\d\\.", "bw")
+						vim.cmd('normal W"ayt:')
 						vim.cmd("wincmd w")
-
-						-- Jump to bottom of buffer
-						vim.cmd("normal! G")
-
-						-- Paste clipboard contents
-						vim.cmd("normal! p")
-
-						-- Comment out the pasted content
-						vim.cmd("normal gcG")
-
-						-- Run the transform command
-						vim.cmd("TransformCode Implement the edits described at the bottom of the file in comments.")
-					end, { desc = "Implement clipboard CodeCompanion edits using ai.nvim.", buffer = 0 })
-
-					-- KEYMAP: gE
-					vim.keymap.set("n", "gE", function()
-						vim.cmd("normal gyge")
-					end, { desc = "Implement CodeCompanion edits under cursor using ai.nvim.", buffer = 0 })
+						vim.cmd("edit <c-r>a<cr>")
+					end, { desc = "Implement clipboard CodeCompanion edits.", buffer = 0 })
 				end,
 			})
 		end,
