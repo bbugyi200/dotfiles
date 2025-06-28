f6649406eea951e9a339f6d0e30caa888b3f30c1
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-06-28 15:09:21 -0400

    ref: Use 'init' instead of 'config'

--- home/dot_config/nvim/lua/plugins/comment.lua
+++ home/dot_config/nvim/lua/plugins/comment.lua
@@ -2,9 +2,8 @@ return {
 	-- PLUGIN: http://github.com/numToStr/Comment.nvim
 	{
 		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-
+		opts = {},
+		init = function()
 			-- Configure SCSS files to use line comments instead of block comments
 			local ft = require("Comment.ft")
 			ft.set("scss", "// %s")
