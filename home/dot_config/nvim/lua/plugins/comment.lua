510b7d250479e98280aee5f89a8151c9ac082a75
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-06-28 15:08:11 -0400

    feat: Use '//' for comments in *.scss files

--- home/dot_config/nvim/lua/plugins/comment.lua
+++ home/dot_config/nvim/lua/plugins/comment.lua
@@ -1,6 +1,13 @@
---- Smart and powerful comment plugin for neovim.
-
 return {
 	-- PLUGIN: http://github.com/numToStr/Comment.nvim
-	{ "numToStr/Comment.nvim", opts = {} },
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+
+			-- Configure SCSS files to use line comments instead of block comments
+			local ft = require("Comment.ft")
+			ft.set("scss", "// %s")
+		end,
+	},
 }
