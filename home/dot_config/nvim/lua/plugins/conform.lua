49a0b61c1e19ac1f618d06dccd1fa4dc97b31d70
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-06-30 14:00:40 -0400

    fix: keep-sorted conform formatter - #5

--- home/dot_config/nvim/lua/plugins/conform.lua
+++ home/dot_config/nvim/lua/plugins/conform.lua
@@ -33,7 +33,7 @@ local function get_conform_opts()
 				c = { "clang_format" },
 				cpp = { "clang_format" },
 				css = { "prettier" },
-				dart = { "dartfmt", "keep_sorted", "tidy_dart" },
+				dart = { "keep_sorted", "dartfmt", "tidy_dart" },
 				gcl = { "gclfmt" },
 				go = { "gofmt" },
 				googlesql = { "format_sql" },
