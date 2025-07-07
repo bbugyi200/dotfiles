43d984fa38adb2253370ed2aacfdd983358b5689
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-07-07 11:47:47 -0400

    feat: Remove truncateline plugin

--- home/dot_config/nvim/lua/plugins/truncateline.lua
+++
@@ -1,9 +0,0 @@
---- Show the start of long lines when scrolling way off to the right.
-
-return {
-	-- PLUGIN: http://github.com/rlychrisg/truncateline.nvim
-	{
-		"rlychrisg/truncateline.nvim",
-		opts = { enabled_on_start = true },
-	},
-}
