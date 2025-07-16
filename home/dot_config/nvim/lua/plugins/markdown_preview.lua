cb91f5df0a9e661b598c0b48e64c547e8b4a1079
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-07-16 11:23:24 -0400

    fix: Add 'bugged' as ft supported by markdown-preview.nvim

--- home/dot_config/nvim/lua/plugins/markdown_preview.lua
+++ home/dot_config/nvim/lua/plugins/markdown_preview.lua
@@ -6,7 +6,7 @@ return {
 	-- See https://github.com/iamcco/markdown-preview.nvim/issues/690#issuecomment-2264399766
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	ft = { "codecompanion", "markdown" },
+	ft = { "bugged", "codecompanion", "markdown" },
 	build = function(plugin)
 		if vim.fn.executable("npx") then
 			vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
@@ -17,7 +17,7 @@ return {
 	end,
 	init = function()
 		if vim.fn.executable("npx") then
-			vim.g.mkdp_filetypes = { "codecompanion", "markdown" }
+			vim.g.mkdp_filetypes = { "bugged", "codecompanion", "markdown" }
 		end
 
 		-- KEYMAP: <leader>mdp
