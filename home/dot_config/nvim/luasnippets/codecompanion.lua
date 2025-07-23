7eb4f17043c4cdc118b3f98dda23f81f4609da08
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-07-23 11:12:55 -0400

    feat: Add 'helpp' cc snippet

--- home/dot_config/nvim/luasnippets/codecompanion.lua
+++ home/dot_config/nvim/luasnippets/codecompanion.lua
@@ -92,6 +92,8 @@ return {
 	s({ trig = "fixx", desc = "Can you help me fix these?" }, { t("Can you help me fix these?") }),
 	-- SNIPPET: help
 	s({ trig = "help", desc = "Can you help me...?" }, { t("Can you help me "), i(1), t("?") }),
+	-- SNIPPET: helpp
+	s({ trig = "helpp", desc = "Can you now help me...?" }, { t("Can you now help me "), i(1), t("?") }),
 	-- SNIPPET: impl
 	s({ trig = "impl", desc = "Can you help me implement...?" }, { t("Can you help me implement "), i(1), t("?") }),
 	-- SNIPPET: plan
