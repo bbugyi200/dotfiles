288925bcbfd3f6faa86bbc586333cc9a649c1bb1
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-08-02 12:57:55 -0400

    feat: Stop copying visual selection when pasting

--- home/dot_config/nvim/lua/config/keymaps/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps/keymaps.lua
@@ -134,3 +134,11 @@ vim.keymap.set(
 vim.keymap.set("n", "<leader>S", "<cmd>noautocmd w<cr>", {
 	desc = "Save current file without running autocmds.",
 })
+
+-- KEYMAP: p
+vim.keymap.set(
+	"x",
+	"p",
+	'"_dP',
+	{ desc = "Paste over visual selection WITHOUT copying visual selection.", noremap = true, silent = true }
+)
