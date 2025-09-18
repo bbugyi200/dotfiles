37288c02bfd89bfb196f19a1ae8f1b75be3d11c9
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-09-18 10:47:35 -0400

    Revert "ref: Migrate to vim.lsp.config"
    This reverts commit 3d303a6216fb86f35a8312892618c42ccf10fbf2.

--- home/dot_config/nvim/lua/plugins/lspconfig.lua
+++ home/dot_config/nvim/lua/plugins/lspconfig.lua
@@ -8,13 +8,15 @@ return {
 		dependencies = {
 			"onsails/lspkind.nvim",
 		},
-		config = function()
+		init = function()
 			local bb = require("bb_utils")
+			local cfg = require("lspconfig")
 
 			if bb.is_goog_machine() then
+				-- CiderLSP
+				local configs = require("lspconfig.configs")
 				local capabilities =
 					require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
 				-- ─────────────────────────── configure ciderlsp ───────────────────────────
 				local ciderlsp_filetypes = {
 					"bzl",
@@ -37,36 +39,45 @@ return {
 					"txt",
 				}
 
-				vim.lsp.config("ciderlsp", {
-					cmd = {
-						"/google/bin/releases/cider/ciderlsp/ciderlsp",
-						"--tooltag=nvim-cmp",
-						"--noforward_sync_responses",
+				configs.ciderlsp = {
+					default_config = {
+						cmd = {
+							"/google/bin/releases/cider/ciderlsp/ciderlsp",
+							"--tooltag=nvim-cmp",
+							"--noforward_sync_responses",
+						},
+						filetypes = ciderlsp_filetypes,
+						root_dir = cfg.util.root_pattern(".citc"),
+						settings = {},
 					},
-					filetypes = ciderlsp_filetypes,
-					root_dir = function(fname)
-						return vim.fs.find(".citc", { path = fname, upward = true })[1]
-					end,
-					capabilities = capabilities,
-				})
-				vim.lsp.enable("ciderlsp")
+				}
 
-				-- ───────────────────────── configure analysislsp ──────────────────────
-				vim.lsp.config("analysislsp", {
-					cmd = {
-						"/google/bin/users/lerm/glint-ale/analysis_lsp/server",
-						"--lint_on_save=false",
-						"--max_qps=10",
-					},
-					filetypes = ciderlsp_filetypes,
-					root_dir = function(fname)
-						return string.match(fname, "(/google/src/cloud/[%w_-]+/[%w_-]+/).+$")
-					end,
+				cfg.ciderlsp.setup({
 					capabilities = capabilities,
 				})
-				vim.lsp.enable("analysislsp")
+
+				-- ───────────────────────── confgure analysislsp ──────────────────────
+				configs.analysislsp = {
+					default_config = {
+						cmd = {
+							"/google/bin/users/lerm/glint-ale/analysis_lsp/server",
+							"--lint_on_save=false",
+							"--max_qps=10",
+						},
+						filetypes = ciderlsp_filetypes,
+						-- root_dir = lspconfig.util.root_pattern('BUILD'),
+						root_dir = function(fname)
+							return string.match(fname, "(/google/src/cloud/[%w_-]+/[%w_-]+/).+$")
+						end,
+						settings = {},
+					},
+				}
+
+				cfg.analysislsp.setup({
+					capabilities = capabilities,
+				})
 			else
-				vim.lsp.config("jedi_language_server", {
+				cfg.jedi_language_server.setup({
 					init_options = {
 						completion = {
 							disableSnippets = false,
@@ -80,9 +91,7 @@ return {
 						},
 					},
 				})
-				vim.lsp.enable("jedi_language_server")
-
-				vim.lsp.config("ruff", {
+				cfg.ruff.setup({
 					init_options = {
 						settings = {
 							-- Any extra CLI arguments for `ruff` go here.
@@ -90,22 +99,20 @@ return {
 						},
 					},
 				})
-				vim.lsp.enable("ruff")
 			end
 
 			-- bash-language-server
-			vim.lsp.config("bashls", {
+			cfg.bashls.setup({
 				filetypes = { "bash", "sh", "sh.chezmoitmpl", "zsh" },
 				cmd = { "bash-language-server", "start" },
 			})
-			vim.lsp.enable("bashls")
 
 			-- lua-language-server
-			vim.lsp.config("lua_ls", {
+			cfg.lua_ls.setup({
 				on_init = function(client)
 					if client.workspace_folders then
 						local path = client.workspace_folders[1].name
-						if vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc") then
+						if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
 							return
 						end
 					end
@@ -132,18 +139,13 @@ return {
 					Lua = {},
 				},
 			})
-			vim.lsp.enable("lua_ls")
 
 			-- vim-language-server
-			vim.lsp.config("vimls", {
+			cfg.vimls.setup({
 				cmd = { "vim-language-server", "--stdio" },
 				filetypes = { "vim" },
-				root_dir = function(fname)
-					local patterns = { "vimrc", ".vimrc", "package.json", ".git" }
-					return vim.fs.find(patterns, { path = fname, upward = true })[1]
-				end,
+				root_dir = cfg.util.root_pattern("vimrc", ".vimrc", "package.json", ".git"),
 			})
-			vim.lsp.enable("vimls")
 		end,
 	},
 }
