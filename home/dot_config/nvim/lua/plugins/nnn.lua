6a78c6a00319bfb2843c3bc6db3711bf1b791e23
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-09-24 11:22:04 -0400

    feat: Add nnn keymaps

--- home/dot_config/nvim/lua/plugins/nnn.lua
+++ home/dot_config/nvim/lua/plugins/nnn.lua
@@ -4,8 +4,24 @@ return {
 	-- PLUGIN: http://github.com/luukvbaal/nnn.nvim
 	{
 		"luukvbaal/nnn.nvim",
-		opts = {},
 		init = function()
+			local builtin = require("nnn").builtin
+			local mappings = {
+				{ "<C-t>", builtin.open_in_tab }, -- open file(s) in tab
+				{ "<C-s>", builtin.open_in_split }, -- open file(s) in split
+				{ "<C-v>", builtin.open_in_vsplit }, -- open file(s) in vertical split
+				{ "<C-p>", builtin.open_in_preview }, -- open file in preview split keeping nnn focused
+				{ "<C-y>", builtin.copy_to_clipboard }, -- copy file(s) to clipboard
+				{ "<C-w>", builtin.cd_to_path }, -- cd to file directory
+				{ "<C-e>", builtin.populate_cmdline }, -- populate cmdline (:) with file(s)
+			}
+			require("nnn").setup({
+				mappings = mappings,
+			})
+
+			-- ╭─────────────────────────────────────────────────────────╮
+			-- │                         KEYMAPS                         │
+			-- ╰─────────────────────────────────────────────────────────╯
 			-- KEYMAP: <leader>-
 			vim.keymap.set("n", "<leader>-", "<cmd>NnnPicker %p:h<cr>", { desc = "Pick a local file with `nnn`." })
 			-- KEYMAP: <leader>nn
