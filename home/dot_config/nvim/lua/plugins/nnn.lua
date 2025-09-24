9889ec0deb68d9fd4dd579dd5f78474d28527cc2
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-09-24 11:24:14 -0400

    feat: Add nnn keymaps - #2

--- home/dot_config/nvim/lua/plugins/nnn.lua
+++ home/dot_config/nvim/lua/plugins/nnn.lua
@@ -6,17 +6,16 @@ return {
 		"luukvbaal/nnn.nvim",
 		init = function()
 			local builtin = require("nnn").builtin
-			local mappings = {
-				{ "<C-t>", builtin.open_in_tab }, -- open file(s) in tab
-				{ "<C-s>", builtin.open_in_split }, -- open file(s) in split
-				{ "<C-v>", builtin.open_in_vsplit }, -- open file(s) in vertical split
-				{ "<C-p>", builtin.open_in_preview }, -- open file in preview split keeping nnn focused
-				{ "<C-y>", builtin.copy_to_clipboard }, -- copy file(s) to clipboard
-				{ "<C-w>", builtin.cd_to_path }, -- cd to file directory
-				{ "<C-e>", builtin.populate_cmdline }, -- populate cmdline (:) with file(s)
-			}
 			require("nnn").setup({
-				mappings = mappings,
+				mappings = {
+					{ "<C-t>", builtin.open_in_tab }, -- open file(s) in tab
+					{ "<C-s>", builtin.open_in_split }, -- open file(s) in split
+					{ "<C-v>", builtin.open_in_vsplit }, -- open file(s) in vertical split
+					{ "<C-p>", builtin.open_in_preview }, -- open file in preview split keeping nnn focused
+					{ "<C-y>", builtin.copy_to_clipboard }, -- copy file(s) to clipboard
+					{ "<C-w>", builtin.cd_to_path }, -- cd to file directory
+					{ "<C-e>", builtin.populate_cmdline }, -- populate cmdline (:) with file(s)
+				},
 			})
 
 			-- ╭─────────────────────────────────────────────────────────╮
