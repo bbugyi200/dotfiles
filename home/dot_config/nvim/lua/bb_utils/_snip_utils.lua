78daaa3d347f76831a2ed6dcf5d73365969534cc
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-09-26 18:58:07 -0400

    feat: Move all cc snippets to md snippets for now

--- home/dot_config/nvim/lua/bb_utils/_snip_utils.lua
+++ home/dot_config/nvim/lua/bb_utils/_snip_utils.lua
@@ -57,8 +57,81 @@ function M.get_markdown_snippets()
 	local s = ls.snippet
 	local i = ls.insert_node
 	local fmt = require("luasnip.extras.fmt").fmt
+	local editor_tool_name = "{insert_edit_into_file}"
 
 	return {
+		-- SNIPPET: ?
+		s({ trig = "?", desc = "Can you ...?" }, { t("Can you "), i(1), t("?") }),
+		-- SNIPPET: @e
+		s({
+			trig = "@e",
+			desc = "Auto-snippet for @" .. editor_tool_name,
+			snippetType = "autosnippet",
+			hidden = true,
+		}, { t("@" .. editor_tool_name) }),
+		-- SNIPPET: @@u
+		s({
+			trig = "@@u",
+			desc = "Use @" .. editor_tool_name .. " to...",
+			snippetType = "autosnippet",
+			hidden = true,
+		}, {
+			t("Use @" .. editor_tool_name .. " to "),
+			i(1),
+			t(
+				". Output calls to the "
+					.. editor_tool_name
+					.. "() function provided by the "
+					.. editor_tool_name
+					.. " tool."
+			),
+		}),
+		-- SNIPPET: @u
+		s({
+			trig = "@u",
+			desc = "Use @" .. editor_tool_name .. " on...",
+			snippetType = "autosnippet",
+			hidden = true,
+		}, { t("Use @" .. editor_tool_name .. " on ") }),
+		-- SNIPPET: @@U
+		s({
+			trig = "@@U",
+			desc = "Use @" .. editor_tool_name .. " on #{buffer}{watch} to...",
+			snippetType = "autosnippet",
+			hidden = true,
+		}, {
+			t("Use @" .. editor_tool_name .. " on #{buffer}{watch} to "),
+			i(1),
+			t(
+				". Output calls to the "
+					.. editor_tool_name
+					.. "() function provided by the "
+					.. editor_tool_name
+					.. " tool."
+			),
+		}),
+		-- SNIPPET: @U
+		s({
+			trig = "@U",
+			desc = "Use @" .. editor_tool_name .. " on #{buffer}{watch} to...",
+			snippetType = "autosnippet",
+			hidden = true,
+		}, { t("Use @" .. editor_tool_name .. " on #{buffer}{watch} to ") }),
+		-- SNIPPET: #b
+		s({
+			trig = "#b",
+			desc = "Auto-snippet for #{buffer}{watch}...",
+			snippetType = "autosnippet",
+			hidden = true,
+		}, { t("#{buffer}{watch}") }),
+		-- SNIPPET: bld
+		s({ trig = "bld", desc = "Build failure" }, {
+			t("Can you help me fix this build (see the build"),
+			i(1),
+			t(".txt file)? When you're done, run the `rabbit build "),
+			i(2),
+			t("` command, then fix the new failures (if any), and repeat until the command is successful."),
+		}),
 		-- SNIPPET: cps
 		s(
 			{ trig = "cps", desc = "Context / Problem / Solution" },
@@ -80,6 +153,67 @@ function M.get_markdown_snippets()
 				}
 			)
 		),
+		-- SNIPPET: cr
+		s(
+			{ trig = "cr", desc = "Critique change request help." },
+			{ t("Can you help me address the Critique comments?") }
+		),
+		-- SNIPPET: di
+		s({ trig = "di", desc = "diff_*.txt file explanation" }, {
+			t("The diff"),
+			i(1),
+			t("."),
+			i(2, "txt"),
+			t(" file contains a diff of the changes made by "),
+			i(3, "the current CL"),
+			t("."),
+			i(4),
+		}),
+		-- SNIPPET: er
+		s(
+			{ trig = "er", desc = "err_*.txt file explanation" },
+			{ t("The err"), i(1), t(".txt file contains "), i(2, "the failure output"), t(".") }
+		),
+		-- SNIPPET: fi
+		s({ trig = "fi", desc = "the ____ file" }, { t("the "), i(1), t(" file") }),
+		-- SNIPPET: fii
+		s({ trig = "fii", desc = "The ____ file" }, { t("The "), i(1), t(" file") }),
+		-- SNIPPET: fix
+		s({ trig = "fix", desc = "Can you help me fix this?" }, { t("Can you help me fix this?") }),
+		-- SNIPPET: fixx
+		s({ trig = "fixx", desc = "Can you help me fix these?" }, { t("Can you help me fix these?") }),
+		-- SNIPPET: help
+		s({ trig = "help", desc = "Can you help me...?" }, { t("Can you help me "), i(1), t("?") }),
+		-- SNIPPET: helpp
+		s({ trig = "helpp", desc = "Can you now help me...?" }, { t("Can you now help me "), i(1), t("?") }),
+		-- SNIPPET: impl
+		s({ trig = "impl", desc = "Can you help me implement...?" }, { t("Can you help me implement "), i(1), t("?") }),
+		-- SNIPPET: iow
+		s({ trig = "iow", desc = "In other words, _____ ..." }, { t("In other words, "), i(1) }),
+		-- SNIPPET: loop
+		s({ trig = "loop", desc = "Repeatedly run a command until it passed." }, {
+			t("When you're done, run the `"),
+			i(1),
+			t("` command, then fix the new failures (if any), and repeat until the command is successful."),
+		}),
+		-- SNIPPET: try
+		s(
+			{ trig = "try", desc = "Try again now that you have access to the ______ file." },
+			{ t("Try again now that you have access to the "), i(1), t(" file.") }
+		),
+		-- SNIPPET: tryy
+		s(
+			{ trig = "tryy", desc = "Try again now that you have access to some more files." },
+			{ t("Try again now that you have access to some more files.") }
+		),
+		-- SNIPPET: tst
+		s({ trig = "tst", desc = "Test failure" }, {
+			t("Can you help me fix this test (see the test"),
+			i(1),
+			t(".txt file)? When you're done, run the `rabbit test -c opt "),
+			i(2),
+			t("` command, then fix the new failures (if any), and repeat until the command is successful."),
+		}),
 	}
 end
 
