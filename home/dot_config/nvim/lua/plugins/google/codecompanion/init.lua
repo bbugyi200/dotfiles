395e943cf34fb59ed42a05ce96311dba263416ac
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-10-08 12:34:51 -0400

    feat: Add --approval-mode yolo - #3

--- home/dot_config/nvim/lua/plugins/google/codecompanion/init.lua
+++ home/dot_config/nvim/lua/plugins/google/codecompanion/init.lua
@@ -57,6 +57,7 @@ return vim.tbl_deep_extend("force", cc.common_plugin_config, {
 									default = {
 										"/google/bin/releases/gemini-cli/tools/gemini",
 										"--approval-mode=yolo",
+										"--yolo",
 										"--gfg",
 										"--experimental-acp",
 										"--port=" .. available_port,
