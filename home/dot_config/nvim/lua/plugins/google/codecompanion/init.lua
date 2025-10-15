524324cd154250457f5a2eaa964f1a04604cf3f0
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-10-15 14:13:53 -0400

    fix: Use --yolo instead of --approval-mode=yolo

--- home/dot_config/nvim/lua/plugins/google/codecompanion/init.lua
+++ home/dot_config/nvim/lua/plugins/google/codecompanion/init.lua
@@ -76,7 +76,7 @@ return vim.tbl_deep_extend("force", cc.common_plugin_config, {
 								commands = {
 									default = {
 										"/google/bin/releases/gemini-cli/tools/gemini",
-										"--approval-mode=yolo",
+										"--yolo",
 										"--gfg",
 										"--experimental-acp",
 										"--port=" .. available_port,
