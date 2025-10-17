5a8e10c1ee4364259585174f63dead4bdc18d3d5
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-10-17 16:47:24 -0400

    fix: Add 'rabbit' to gemini allowed tools

--- home/dot_config/nvim/lua/plugins/google/codecompanion/init.lua
+++ home/dot_config/nvim/lua/plugins/google/codecompanion/init.lua
@@ -79,6 +79,7 @@ return vim.tbl_deep_extend("force", cc.common_plugin_config, {
 										"--gfg",
 										"--experimental-acp",
 										"--port=" .. available_port,
+										"--allowed-tools=ShellTool(rabbit)",
 									},
 								},
 								defaults = {
