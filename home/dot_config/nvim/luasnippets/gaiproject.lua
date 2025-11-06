309fca189dd44177882884f0afef58ddff423d69
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-11-06 14:02:14 -0500

    feat: Add gaiproject snippets

---
+++ home/dot_config/nvim/luasnippets/gaiproject.lua
@@ -0,0 +1,17 @@
+-- Snippets for ~/.gai/projects/*.md files live here.
+
+return {
+	-- SNIPPET: cs
+	s({ trig = "cs", desc = "ChangeSpec template (without TEST TARGETS field)" }, {
+		t("NAME: "),
+		i(1),
+		t({ "", "DESCRIPTION:", "  " }),
+		i(2, "Title"),
+		t({ "", "", "  " }),
+		i(3, "Body"),
+		t({ "", "PARENT: None", "CL: None", "STATUS: Not Started" }),
+		i(0),
+	}),
+	-- SNIPPET: tt
+	s({ trig = "tt", desc = "TEST TARGETS field" }, { t("TEST TARGETS: "), i(0) }),
+}
