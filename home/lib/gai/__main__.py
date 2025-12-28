46cda94a16fb8ddb773528616663e58189c3248d
Bryan Bugyi <bryanbugyi34@gmail.com>
2025-12-28 18:13:32 -0500

    fix: pybash to handle main/ - #3

---
+++ home/lib/gai/__main__.py
@@ -0,0 +1,6 @@
+"""Allow running the package as a module: python -m gai"""
+
+from main.entry import main
+
+if __name__ == "__main__":
+    main()
--- home/lib/gai/main/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Allow running the package as a module: python -m main"""
-
-from .entry import main
-
-if __name__ == "__main__":
-    main()
