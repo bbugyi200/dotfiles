aaf4d41d7af9828e856807872b2c8c8aa19ff44a
Bryan Bugyi <bryanbugyi34@gmail.com>
2026-03-23 16:52:42 -0400

    feat: Support project-local .nvim.lua config files
    Add DirChanged autocmd to source .nvim.lua from cwd on startup and
    directory change. Add .nvim.lua to global gitignore.

--- home/dot_config/nvim/lua/config/load_local_configs.lua
+++ home/dot_config/nvim/lua/config/load_local_configs.lua
@@ -1,9 +1,9 @@
 --- Source local vimrc / init.lua files.
 --
 -- P2: Improve local vimrc
---   [ ] Add autocmd for $(pwd)/.vimrc.local
+--   [x] Add autocmd for $(pwd)/.nvim.lua
 --   [ ] Move ~/.vimrc.local to ~/etc/vimrc?
---   [ ] Support init.lua equivalents to the above!
+--   [x] Support init.lua equivalents to the above!
 
 --- Source {file}, if it exists.
 ---
@@ -16,3 +16,13 @@ local function source_if_exists(file)
 end
 
 source_if_exists(vim.env.HOME .. "/.vimrc.local")
+
+-- Source .nvim.lua from the current working directory (and re-source on :cd).
+source_if_exists(vim.fn.getcwd() .. "/.nvim.lua")
+
+vim.api.nvim_create_autocmd("DirChanged", {
+	desc = "Source .nvim.lua when changing directories",
+	callback = function()
+		source_if_exists(vim.fn.getcwd() .. "/.nvim.lua")
+	end,
+})
