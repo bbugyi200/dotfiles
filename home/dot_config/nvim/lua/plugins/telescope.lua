d9e4d8be0b7ad3e67447e57aa0d0c201a0e70125
Bryan Bugyi <bryanbugyi34@gmail.com>
2026-03-30 16:50:07 -0400

    fix: Switch telescope.nvim to master branch for Neovim 0.12 compat
    The 0.1.x branch calls nvim-treesitter's removed ft_to_lang() function,
    causing errors in the previewer. The master branch uses the built-in
    vim.treesitter.language.get_lang() API instead.

--- home/dot_config/nvim/lua/plugins/telescope.lua
+++ home/dot_config/nvim/lua/plugins/telescope.lua
@@ -75,7 +75,7 @@ return {
 	-- PLUGIN: http://github.com/nvim-telescope/telescope.nvim
 	{
 		telescope_plugin_name,
-		branch = "0.1.x",
+		branch = "master",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local lga_actions = require("telescope-live-grep-args.actions")
